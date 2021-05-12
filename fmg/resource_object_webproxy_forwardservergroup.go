906a987f60fc1ad2e389b5ef7ceb72028dc39e80
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:49:30 -0400

    Add resource_object_webproxy_forwardservergroup

---
+++ fmg/resource_object_webproxy_forwardservergroup.go
@@ -0,0 +1,471 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Configure a forward server group consisting or multiple forward servers. Supports failover and load balancing.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectWebProxyForwardServerGroup() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectWebProxyForwardServerGroupCreate,
+		Read:   resourceObjectWebProxyForwardServerGroupRead,
+		Update: resourceObjectWebProxyForwardServerGroupUpdate,
+		Delete: resourceObjectWebProxyForwardServerGroupDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"affinity": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"group_down_option": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"ldb_method": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"server_list": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"weight": &schema.Schema{
+							Type:     schema.TypeInt,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"dynamic_sort_subtable": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "false",
+			},
+		},
+	}
+}
+
+func resourceObjectWebProxyForwardServerGroupCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWebProxyForwardServerGroup(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWebProxyForwardServerGroup resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectWebProxyForwardServerGroup(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWebProxyForwardServerGroup resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectWebProxyForwardServerGroupRead(d, m)
+}
+
+func resourceObjectWebProxyForwardServerGroupUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWebProxyForwardServerGroup(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWebProxyForwardServerGroup resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectWebProxyForwardServerGroup(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWebProxyForwardServerGroup resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectWebProxyForwardServerGroupRead(d, m)
+}
+
+func resourceObjectWebProxyForwardServerGroupDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectWebProxyForwardServerGroup(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectWebProxyForwardServerGroup resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectWebProxyForwardServerGroupRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectWebProxyForwardServerGroup(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWebProxyForwardServerGroup resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectWebProxyForwardServerGroup(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWebProxyForwardServerGroup resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectWebProxyForwardServerGroupAffinity(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "disable",
+			1: "enable",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectWebProxyForwardServerGroupGroupDownOption(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "block",
+			1: "pass",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectWebProxyForwardServerGroupLdbMethod(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			2: "weighted",
+			3: "least-session",
+			7: "active-passive",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectWebProxyForwardServerGroupName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWebProxyForwardServerGroupServerList(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			v := flattenObjectWebProxyForwardServerGroupServerListName(i["name"], d, pre_append)
+			tmp["name"] = fortiAPISubPartPatch(v, "ObjectWebProxyForwardServerGroup-ServerList-Name")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "weight"
+		if _, ok := i["weight"]; ok {
+			v := flattenObjectWebProxyForwardServerGroupServerListWeight(i["weight"], d, pre_append)
+			tmp["weight"] = fortiAPISubPartPatch(v, "ObjectWebProxyForwardServerGroup-ServerList-Weight")
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenObjectWebProxyForwardServerGroupServerListName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWebProxyForwardServerGroupServerListWeight(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectObjectWebProxyForwardServerGroup(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("affinity", flattenObjectWebProxyForwardServerGroupAffinity(o["affinity"], d, "affinity")); err != nil {
+		if vv, ok := fortiAPIPatch(o["affinity"], "ObjectWebProxyForwardServerGroup-Affinity"); ok {
+			if err = d.Set("affinity", vv); err != nil {
+				return fmt.Errorf("Error reading affinity: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading affinity: %v", err)
+		}
+	}
+
+	if err = d.Set("group_down_option", flattenObjectWebProxyForwardServerGroupGroupDownOption(o["group-down-option"], d, "group_down_option")); err != nil {
+		if vv, ok := fortiAPIPatch(o["group-down-option"], "ObjectWebProxyForwardServerGroup-GroupDownOption"); ok {
+			if err = d.Set("group_down_option", vv); err != nil {
+				return fmt.Errorf("Error reading group_down_option: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading group_down_option: %v", err)
+		}
+	}
+
+	if err = d.Set("ldb_method", flattenObjectWebProxyForwardServerGroupLdbMethod(o["ldb-method"], d, "ldb_method")); err != nil {
+		if vv, ok := fortiAPIPatch(o["ldb-method"], "ObjectWebProxyForwardServerGroup-LdbMethod"); ok {
+			if err = d.Set("ldb_method", vv); err != nil {
+				return fmt.Errorf("Error reading ldb_method: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading ldb_method: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenObjectWebProxyForwardServerGroupName(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "ObjectWebProxyForwardServerGroup-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("server_list", flattenObjectWebProxyForwardServerGroupServerList(o["server-list"], d, "server_list")); err != nil {
+			if vv, ok := fortiAPIPatch(o["server-list"], "ObjectWebProxyForwardServerGroup-ServerList"); ok {
+				if err = d.Set("server_list", vv); err != nil {
+					return fmt.Errorf("Error reading server_list: %v", err)
+				}
+			} else {
+				return fmt.Errorf("Error reading server_list: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("server_list"); ok {
+			if err = d.Set("server_list", flattenObjectWebProxyForwardServerGroupServerList(o["server-list"], d, "server_list")); err != nil {
+				if vv, ok := fortiAPIPatch(o["server-list"], "ObjectWebProxyForwardServerGroup-ServerList"); ok {
+					if err = d.Set("server_list", vv); err != nil {
+						return fmt.Errorf("Error reading server_list: %v", err)
+					}
+				} else {
+					return fmt.Errorf("Error reading server_list: %v", err)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectWebProxyForwardServerGroupFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectWebProxyForwardServerGroupAffinity(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWebProxyForwardServerGroupGroupDownOption(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWebProxyForwardServerGroupLdbMethod(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWebProxyForwardServerGroupName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWebProxyForwardServerGroupServerList(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["name"], _ = expandObjectWebProxyForwardServerGroupServerListName(d, i["name"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "weight"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["weight"], _ = expandObjectWebProxyForwardServerGroupServerListWeight(d, i["weight"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandObjectWebProxyForwardServerGroupServerListName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWebProxyForwardServerGroupServerListWeight(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectWebProxyForwardServerGroup(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("affinity"); ok {
+		t, err := expandObjectWebProxyForwardServerGroupAffinity(d, v, "affinity")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["affinity"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("group_down_option"); ok {
+		t, err := expandObjectWebProxyForwardServerGroupGroupDownOption(d, v, "group_down_option")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["group-down-option"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("ldb_method"); ok {
+		t, err := expandObjectWebProxyForwardServerGroupLdbMethod(d, v, "ldb_method")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["ldb-method"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandObjectWebProxyForwardServerGroupName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("server_list"); ok {
+		t, err := expandObjectWebProxyForwardServerGroupServerList(d, v, "server_list")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["server-list"] = t
+		}
+	}
+
+	return &obj, nil
+}
