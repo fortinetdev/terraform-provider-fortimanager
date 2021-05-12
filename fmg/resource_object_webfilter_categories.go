7523b8e932b55d30c9bba07a4589224ed341fc5b
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:49:25 -0400

    Add resource_object_webfilter_categories

---
+++ fmg/resource_object_webfilter_categories.go
@@ -0,0 +1,206 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: ObjectWebfilter Categories
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectWebfilterCategories() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectWebfilterCategoriesCreate,
+		Read:   resourceObjectWebfilterCategoriesRead,
+		Update: resourceObjectWebfilterCategoriesUpdate,
+		Delete: resourceObjectWebfilterCategoriesDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"fosid": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceObjectWebfilterCategoriesCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWebfilterCategories(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWebfilterCategories resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectWebfilterCategories(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWebfilterCategories resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, "fosid"))
+
+	return resourceObjectWebfilterCategoriesRead(d, m)
+}
+
+func resourceObjectWebfilterCategoriesUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWebfilterCategories(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWebfilterCategories resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectWebfilterCategories(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWebfilterCategories resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, "fosid"))
+
+	return resourceObjectWebfilterCategoriesRead(d, m)
+}
+
+func resourceObjectWebfilterCategoriesDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectWebfilterCategories(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectWebfilterCategories resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectWebfilterCategoriesRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectWebfilterCategories(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWebfilterCategories resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectWebfilterCategories(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWebfilterCategories resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectWebfilterCategoriesId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectObjectWebfilterCategories(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("fosid", flattenObjectWebfilterCategoriesId(o["id"], d, "fosid")); err != nil {
+		if vv, ok := fortiAPIPatch(o["id"], "ObjectWebfilterCategories-Id"); ok {
+			if err = d.Set("fosid", vv); err != nil {
+				return fmt.Errorf("Error reading fosid: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectWebfilterCategoriesFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectWebfilterCategoriesId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectWebfilterCategories(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("fosid"); ok {
+		t, err := expandObjectWebfilterCategoriesId(d, v, "fosid")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["id"] = t
+		}
+	}
+
+	return &obj, nil
+}
