83c8fcdc2ee60682c0a566b4d925fc6c8eca226b
Frank Shen <fshen01@fortinet.com>
2021-05-12 01:20:47 -0400

    Fix exec_workspace_action comment

--- vendor/github.com/fortinetdev/forti-sdk-go2/fmg/sdkcore/exec_workspace_action.go
+++ vendor/github.com/fortinetdev/forti-sdk-go2/fmg/sdkcore/exec_workspace_action.go
@@ -1,6 +1,6 @@
 package forticlient
 
-// CreateUpdateFirewallSecurityPolicySort API operation for FortiOS to sort the firewall policies.
+// CreateUpdateExecWorkspaceAction API operation for locking ADOMs, devices, or policy packages so that an administrator can prevent other administrators from making changes to the elements that they are working in.
 // Returns error for service API and SDK errors.
 func (c *FortiSDKClient) CreateUpdateExecWorkspaceAction(globaladom, action, target, param string) (output map[string]interface{}, err error) {
 	path := "/dvmdb/[*]/workspace"
