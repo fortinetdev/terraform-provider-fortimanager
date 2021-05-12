b26a0e8d3232878dab9b46e8e02f7aa0c44eefa8
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:50:06 -0400

    Add resource_securityconsole_package_clone

---
+++ fmg/resource_securityconsole_package_clone.go
@@ -0,0 +1,363 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Clone a policy package within the same ADOM.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSecurityconsolePackageClone() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSecurityconsolePackageCloneUpdate,
+		Read:   resourceSecurityconsolePackageCloneRead,
+		Update: resourceSecurityconsolePackageCloneUpdate,
+		Delete: resourceSecurityconsolePackageCloneDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"force_recreate": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Computed: true,
+			},
+			"fmgadom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"dst_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"dst_parent": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"pkg": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"scope": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"vdom": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"dynamic_sort_subtable": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "false",
+			},
+		},
+	}
+}
+
+func resourceSecurityconsolePackageCloneUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "", fmt.Errorf("")
+
+	obj, err := getObjectSecurityconsolePackageClone(d)
+	if err != nil {
+		return fmt.Errorf("Error updating SecurityconsolePackageClone resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateSecurityconsolePackageClone(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating SecurityconsolePackageClone resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId("SecurityconsolePackageClone")
+
+	return resourceSecurityconsolePackageCloneRead(d, m)
+}
+
+func resourceSecurityconsolePackageCloneDelete(d *schema.ResourceData, m interface{}) error {
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSecurityconsolePackageCloneRead(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func flattenSecurityconsolePackageCloneAdom(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSecurityconsolePackageCloneDstName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSecurityconsolePackageCloneDstParent(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSecurityconsolePackageClonePkg(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSecurityconsolePackageCloneScope(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			v := flattenSecurityconsolePackageCloneScopeName(i["name"], d, pre_append)
+			tmp["name"] = fortiAPISubPartPatch(v, "SecurityconsolePackageClone-Scope-Name")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "vdom"
+		if _, ok := i["vdom"]; ok {
+			v := flattenSecurityconsolePackageCloneScopeVdom(i["vdom"], d, pre_append)
+			tmp["vdom"] = fortiAPISubPartPatch(v, "SecurityconsolePackageClone-Scope-Vdom")
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenSecurityconsolePackageCloneScopeName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSecurityconsolePackageCloneScopeVdom(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectSecurityconsolePackageClone(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("fmgadom", flattenSecurityconsolePackageCloneAdom(o["adom"], d, "fmgadom")); err != nil {
+		if vv, ok := fortiAPIPatch(o["adom"], "SecurityconsolePackageClone-Adom"); ok {
+			if err = d.Set("fmgadom", vv); err != nil {
+				return fmt.Errorf("Error reading fmgadom: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading fmgadom: %v", err)
+		}
+	}
+
+	if err = d.Set("dst_name", flattenSecurityconsolePackageCloneDstName(o["dst_name"], d, "dst_name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["dst_name"], "SecurityconsolePackageClone-DstName"); ok {
+			if err = d.Set("dst_name", vv); err != nil {
+				return fmt.Errorf("Error reading dst_name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading dst_name: %v", err)
+		}
+	}
+
+	if err = d.Set("dst_parent", flattenSecurityconsolePackageCloneDstParent(o["dst_parent"], d, "dst_parent")); err != nil {
+		if vv, ok := fortiAPIPatch(o["dst_parent"], "SecurityconsolePackageClone-DstParent"); ok {
+			if err = d.Set("dst_parent", vv); err != nil {
+				return fmt.Errorf("Error reading dst_parent: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading dst_parent: %v", err)
+		}
+	}
+
+	if err = d.Set("pkg", flattenSecurityconsolePackageClonePkg(o["pkg"], d, "pkg")); err != nil {
+		if vv, ok := fortiAPIPatch(o["pkg"], "SecurityconsolePackageClone-Pkg"); ok {
+			if err = d.Set("pkg", vv); err != nil {
+				return fmt.Errorf("Error reading pkg: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading pkg: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("scope", flattenSecurityconsolePackageCloneScope(o["scope"], d, "scope")); err != nil {
+			if vv, ok := fortiAPIPatch(o["scope"], "SecurityconsolePackageClone-Scope"); ok {
+				if err = d.Set("scope", vv); err != nil {
+					return fmt.Errorf("Error reading scope: %v", err)
+				}
+			} else {
+				return fmt.Errorf("Error reading scope: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("scope"); ok {
+			if err = d.Set("scope", flattenSecurityconsolePackageCloneScope(o["scope"], d, "scope")); err != nil {
+				if vv, ok := fortiAPIPatch(o["scope"], "SecurityconsolePackageClone-Scope"); ok {
+					if err = d.Set("scope", vv); err != nil {
+						return fmt.Errorf("Error reading scope: %v", err)
+					}
+				} else {
+					return fmt.Errorf("Error reading scope: %v", err)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func flattenSecurityconsolePackageCloneFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandSecurityconsolePackageCloneAdom(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSecurityconsolePackageCloneDstName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSecurityconsolePackageCloneDstParent(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSecurityconsolePackageClonePkg(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSecurityconsolePackageCloneScope(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["name"], _ = expandSecurityconsolePackageCloneScopeName(d, i["name"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "vdom"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["vdom"], _ = expandSecurityconsolePackageCloneScopeVdom(d, i["vdom"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandSecurityconsolePackageCloneScopeName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSecurityconsolePackageCloneScopeVdom(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectSecurityconsolePackageClone(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("fmgadom"); ok {
+		t, err := expandSecurityconsolePackageCloneAdom(d, v, "fmgadom")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["adom"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("dst_name"); ok {
+		t, err := expandSecurityconsolePackageCloneDstName(d, v, "dst_name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["dst_name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("dst_parent"); ok {
+		t, err := expandSecurityconsolePackageCloneDstParent(d, v, "dst_parent")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["dst_parent"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("pkg"); ok {
+		t, err := expandSecurityconsolePackageClonePkg(d, v, "pkg")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["pkg"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("scope"); ok {
+		t, err := expandSecurityconsolePackageCloneScope(d, v, "scope")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["scope"] = t
+		}
+	}
+
+	return &obj, nil
+}
