6a24ac0357c2d24116eb655b31eec7ed32d780bf
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:48:11 -0400

    Add resource_object_firewall_service_category

---
+++ fmg/resource_object_firewall_service_category.go
@@ -0,0 +1,271 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Configure service categories.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectFirewallServiceCategory() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectFirewallServiceCategoryCreate,
+		Read:   resourceObjectFirewallServiceCategoryRead,
+		Update: resourceObjectFirewallServiceCategoryUpdate,
+		Delete: resourceObjectFirewallServiceCategoryDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"comment": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"global_object": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceObjectFirewallServiceCategoryCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectFirewallServiceCategory(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectFirewallServiceCategory resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectFirewallServiceCategory(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectFirewallServiceCategory resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectFirewallServiceCategoryRead(d, m)
+}
+
+func resourceObjectFirewallServiceCategoryUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectFirewallServiceCategory(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectFirewallServiceCategory resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectFirewallServiceCategory(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectFirewallServiceCategory resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectFirewallServiceCategoryRead(d, m)
+}
+
+func resourceObjectFirewallServiceCategoryDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectFirewallServiceCategory(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectFirewallServiceCategory resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectFirewallServiceCategoryRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectFirewallServiceCategory(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectFirewallServiceCategory resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectFirewallServiceCategory(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectFirewallServiceCategory resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectFirewallServiceCategoryComment(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallServiceCategoryGlobalObject(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallServiceCategoryName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectObjectFirewallServiceCategory(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("comment", flattenObjectFirewallServiceCategoryComment(o["comment"], d, "comment")); err != nil {
+		if vv, ok := fortiAPIPatch(o["comment"], "ObjectFirewallServiceCategory-Comment"); ok {
+			if err = d.Set("comment", vv); err != nil {
+				return fmt.Errorf("Error reading comment: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading comment: %v", err)
+		}
+	}
+
+	if err = d.Set("global_object", flattenObjectFirewallServiceCategoryGlobalObject(o["global-object"], d, "global_object")); err != nil {
+		if vv, ok := fortiAPIPatch(o["global-object"], "ObjectFirewallServiceCategory-GlobalObject"); ok {
+			if err = d.Set("global_object", vv); err != nil {
+				return fmt.Errorf("Error reading global_object: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading global_object: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenObjectFirewallServiceCategoryName(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "ObjectFirewallServiceCategory-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectFirewallServiceCategoryFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectFirewallServiceCategoryComment(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallServiceCategoryGlobalObject(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallServiceCategoryName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectFirewallServiceCategory(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("comment"); ok {
+		t, err := expandObjectFirewallServiceCategoryComment(d, v, "comment")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["comment"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("global_object"); ok {
+		t, err := expandObjectFirewallServiceCategoryGlobalObject(d, v, "global_object")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["global-object"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandObjectFirewallServiceCategoryName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	return &obj, nil
+}
