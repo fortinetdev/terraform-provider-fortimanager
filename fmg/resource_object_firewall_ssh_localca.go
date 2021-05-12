afb3d4491d4d4383b8af84b9b7163c3a987b7d19
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:48:14 -0400

    Add resource_object_firewall_ssh_localca

---
+++ fmg/resource_object_firewall_ssh_localca.go
@@ -0,0 +1,344 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: SSH proxy local CA.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectFirewallSshLocalCa() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectFirewallSshLocalCaCreate,
+		Read:   resourceObjectFirewallSshLocalCaRead,
+		Update: resourceObjectFirewallSshLocalCaUpdate,
+		Delete: resourceObjectFirewallSshLocalCaDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"password": &schema.Schema{
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+				Computed: true,
+			},
+			"private_key": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"public_key": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"source": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceObjectFirewallSshLocalCaCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectFirewallSshLocalCa(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectFirewallSshLocalCa resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectFirewallSshLocalCa(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectFirewallSshLocalCa resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectFirewallSshLocalCaRead(d, m)
+}
+
+func resourceObjectFirewallSshLocalCaUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectFirewallSshLocalCa(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectFirewallSshLocalCa resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectFirewallSshLocalCa(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectFirewallSshLocalCa resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectFirewallSshLocalCaRead(d, m)
+}
+
+func resourceObjectFirewallSshLocalCaDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectFirewallSshLocalCa(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectFirewallSshLocalCa resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectFirewallSshLocalCaRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectFirewallSshLocalCa(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectFirewallSshLocalCa resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectFirewallSshLocalCa(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectFirewallSshLocalCa resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectFirewallSshLocalCaName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallSshLocalCaPassword(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return flattenStringList(v)
+}
+
+func flattenObjectFirewallSshLocalCaPrivateKey(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallSshLocalCaPublicKey(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallSshLocalCaSource(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "built-in",
+			1: "user",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func refreshObjectObjectFirewallSshLocalCa(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", flattenObjectFirewallSshLocalCaName(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "ObjectFirewallSshLocalCa-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("password", flattenObjectFirewallSshLocalCaPassword(o["password"], d, "password")); err != nil {
+		if vv, ok := fortiAPIPatch(o["password"], "ObjectFirewallSshLocalCa-Password"); ok {
+			if err = d.Set("password", vv); err != nil {
+				return fmt.Errorf("Error reading password: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading password: %v", err)
+		}
+	}
+
+	if err = d.Set("private_key", flattenObjectFirewallSshLocalCaPrivateKey(o["private-key"], d, "private_key")); err != nil {
+		if vv, ok := fortiAPIPatch(o["private-key"], "ObjectFirewallSshLocalCa-PrivateKey"); ok {
+			if err = d.Set("private_key", vv); err != nil {
+				return fmt.Errorf("Error reading private_key: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading private_key: %v", err)
+		}
+	}
+
+	if err = d.Set("public_key", flattenObjectFirewallSshLocalCaPublicKey(o["public-key"], d, "public_key")); err != nil {
+		if vv, ok := fortiAPIPatch(o["public-key"], "ObjectFirewallSshLocalCa-PublicKey"); ok {
+			if err = d.Set("public_key", vv); err != nil {
+				return fmt.Errorf("Error reading public_key: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading public_key: %v", err)
+		}
+	}
+
+	if err = d.Set("source", flattenObjectFirewallSshLocalCaSource(o["source"], d, "source")); err != nil {
+		if vv, ok := fortiAPIPatch(o["source"], "ObjectFirewallSshLocalCa-Source"); ok {
+			if err = d.Set("source", vv); err != nil {
+				return fmt.Errorf("Error reading source: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading source: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectFirewallSshLocalCaFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectFirewallSshLocalCaName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallSshLocalCaPassword(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return expandStringList(v.(*schema.Set).List()), nil
+}
+
+func expandObjectFirewallSshLocalCaPrivateKey(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallSshLocalCaPublicKey(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallSshLocalCaSource(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectFirewallSshLocalCa(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandObjectFirewallSshLocalCaName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("password"); ok {
+		t, err := expandObjectFirewallSshLocalCaPassword(d, v, "password")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["password"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("private_key"); ok {
+		t, err := expandObjectFirewallSshLocalCaPrivateKey(d, v, "private_key")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["private-key"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("public_key"); ok {
+		t, err := expandObjectFirewallSshLocalCaPublicKey(d, v, "public_key")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["public-key"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("source"); ok {
+		t, err := expandObjectFirewallSshLocalCaSource(d, v, "source")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["source"] = t
+		}
+	}
+
+	return &obj, nil
+}
