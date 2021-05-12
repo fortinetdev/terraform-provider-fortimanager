b3bfd8cf3b7bf579f7644e81e3cf0c4667b3f72a
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:49:26 -0400

    Add resource_object_webfilter_content

---
+++ fmg/resource_object_webfilter_content.go
@@ -0,0 +1,549 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Configure Web filter banned word table.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectWebfilterContent() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectWebfilterContentCreate,
+		Read:   resourceObjectWebfilterContentRead,
+		Update: resourceObjectWebfilterContentUpdate,
+		Delete: resourceObjectWebfilterContentDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"comment": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"entries": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"action": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"lang": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"pattern_type": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"score": &schema.Schema{
+							Type:     schema.TypeInt,
+							Optional: true,
+							Computed: true,
+						},
+						"status": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"fosid": &schema.Schema{
+				Type:     schema.TypeInt,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"dynamic_sort_subtable": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "false",
+			},
+		},
+	}
+}
+
+func resourceObjectWebfilterContentCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWebfilterContent(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWebfilterContent resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectWebfilterContent(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWebfilterContent resource: %v", err)
+	}
+
+	d.SetId(strconv.Itoa(getIntKey(d, "fosid")))
+
+	return resourceObjectWebfilterContentRead(d, m)
+}
+
+func resourceObjectWebfilterContentUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWebfilterContent(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWebfilterContent resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectWebfilterContent(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWebfilterContent resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(strconv.Itoa(getIntKey(d, "fosid")))
+
+	return resourceObjectWebfilterContentRead(d, m)
+}
+
+func resourceObjectWebfilterContentDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectWebfilterContent(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectWebfilterContent resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectWebfilterContentRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectWebfilterContent(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWebfilterContent resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectWebfilterContent(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWebfilterContent resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectWebfilterContentComment(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWebfilterContentEntries(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "action"
+		if _, ok := i["action"]; ok {
+			v := flattenObjectWebfilterContentEntriesAction(i["action"], d, pre_append)
+			tmp["action"] = fortiAPISubPartPatch(v, "ObjectWebfilterContent-Entries-Action")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "lang"
+		if _, ok := i["lang"]; ok {
+			v := flattenObjectWebfilterContentEntriesLang(i["lang"], d, pre_append)
+			tmp["lang"] = fortiAPISubPartPatch(v, "ObjectWebfilterContent-Entries-Lang")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			v := flattenObjectWebfilterContentEntriesName(i["name"], d, pre_append)
+			tmp["name"] = fortiAPISubPartPatch(v, "ObjectWebfilterContent-Entries-Name")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "pattern_type"
+		if _, ok := i["pattern-type"]; ok {
+			v := flattenObjectWebfilterContentEntriesPatternType(i["pattern-type"], d, pre_append)
+			tmp["pattern_type"] = fortiAPISubPartPatch(v, "ObjectWebfilterContent-Entries-PatternType")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "score"
+		if _, ok := i["score"]; ok {
+			v := flattenObjectWebfilterContentEntriesScore(i["score"], d, pre_append)
+			tmp["score"] = fortiAPISubPartPatch(v, "ObjectWebfilterContent-Entries-Score")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "status"
+		if _, ok := i["status"]; ok {
+			v := flattenObjectWebfilterContentEntriesStatus(i["status"], d, pre_append)
+			tmp["status"] = fortiAPISubPartPatch(v, "ObjectWebfilterContent-Entries-Status")
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenObjectWebfilterContentEntriesAction(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "exempt",
+			1: "block",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectWebfilterContentEntriesLang(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "western",
+			1: "simch",
+			2: "trach",
+			3: "japanese",
+			4: "korean",
+			5: "french",
+			6: "thai",
+			7: "spanish",
+			8: "cyrillic",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectWebfilterContentEntriesName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWebfilterContentEntriesPatternType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "wildcard",
+			1: "regexp",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectWebfilterContentEntriesScore(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWebfilterContentEntriesStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "disable",
+			1: "enable",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectWebfilterContentId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWebfilterContentName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectObjectWebfilterContent(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("comment", flattenObjectWebfilterContentComment(o["comment"], d, "comment")); err != nil {
+		if vv, ok := fortiAPIPatch(o["comment"], "ObjectWebfilterContent-Comment"); ok {
+			if err = d.Set("comment", vv); err != nil {
+				return fmt.Errorf("Error reading comment: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading comment: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("entries", flattenObjectWebfilterContentEntries(o["entries"], d, "entries")); err != nil {
+			if vv, ok := fortiAPIPatch(o["entries"], "ObjectWebfilterContent-Entries"); ok {
+				if err = d.Set("entries", vv); err != nil {
+					return fmt.Errorf("Error reading entries: %v", err)
+				}
+			} else {
+				return fmt.Errorf("Error reading entries: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("entries"); ok {
+			if err = d.Set("entries", flattenObjectWebfilterContentEntries(o["entries"], d, "entries")); err != nil {
+				if vv, ok := fortiAPIPatch(o["entries"], "ObjectWebfilterContent-Entries"); ok {
+					if err = d.Set("entries", vv); err != nil {
+						return fmt.Errorf("Error reading entries: %v", err)
+					}
+				} else {
+					return fmt.Errorf("Error reading entries: %v", err)
+				}
+			}
+		}
+	}
+
+	if err = d.Set("fosid", flattenObjectWebfilterContentId(o["id"], d, "fosid")); err != nil {
+		if vv, ok := fortiAPIPatch(o["id"], "ObjectWebfilterContent-Id"); ok {
+			if err = d.Set("fosid", vv); err != nil {
+				return fmt.Errorf("Error reading fosid: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenObjectWebfilterContentName(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "ObjectWebfilterContent-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectWebfilterContentFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectWebfilterContentComment(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWebfilterContentEntries(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "action"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["action"], _ = expandObjectWebfilterContentEntriesAction(d, i["action"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "lang"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["lang"], _ = expandObjectWebfilterContentEntriesLang(d, i["lang"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["name"], _ = expandObjectWebfilterContentEntriesName(d, i["name"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "pattern_type"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["pattern-type"], _ = expandObjectWebfilterContentEntriesPatternType(d, i["pattern_type"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "score"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["score"], _ = expandObjectWebfilterContentEntriesScore(d, i["score"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "status"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["status"], _ = expandObjectWebfilterContentEntriesStatus(d, i["status"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandObjectWebfilterContentEntriesAction(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWebfilterContentEntriesLang(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWebfilterContentEntriesName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWebfilterContentEntriesPatternType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWebfilterContentEntriesScore(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWebfilterContentEntriesStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWebfilterContentId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWebfilterContentName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectWebfilterContent(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("comment"); ok {
+		t, err := expandObjectWebfilterContentComment(d, v, "comment")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["comment"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("entries"); ok {
+		t, err := expandObjectWebfilterContentEntries(d, v, "entries")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["entries"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("fosid"); ok {
+		t, err := expandObjectWebfilterContentId(d, v, "fosid")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["id"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandObjectWebfilterContentName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	return &obj, nil
+}
