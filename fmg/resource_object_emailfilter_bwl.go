42382a13ab5b642f6ad5292c415b9a2ab00de8b7
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:47:55 -0400

    Add resource_object_emailfilter_bwl

---
+++ fmg/resource_object_emailfilter_bwl.go
@@ -0,0 +1,623 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Configure anti-spam black/white list.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectEmailfilterBwl() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectEmailfilterBwlCreate,
+		Read:   resourceObjectEmailfilterBwlRead,
+		Update: resourceObjectEmailfilterBwlUpdate,
+		Delete: resourceObjectEmailfilterBwlDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"comment": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"entries": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"action": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"addr_type": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"email_pattern": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"id": &schema.Schema{
+							Type:     schema.TypeInt,
+							Optional: true,
+							Computed: true,
+						},
+						"ip4_subnet": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"ip6_subnet": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"pattern_type": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"status": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"type": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"fosid": &schema.Schema{
+				Type:     schema.TypeInt,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"dynamic_sort_subtable": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "false",
+			},
+		},
+	}
+}
+
+func resourceObjectEmailfilterBwlCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectEmailfilterBwl(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectEmailfilterBwl resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectEmailfilterBwl(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectEmailfilterBwl resource: %v", err)
+	}
+
+	d.SetId(strconv.Itoa(getIntKey(d, "fosid")))
+
+	return resourceObjectEmailfilterBwlRead(d, m)
+}
+
+func resourceObjectEmailfilterBwlUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectEmailfilterBwl(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectEmailfilterBwl resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectEmailfilterBwl(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectEmailfilterBwl resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(strconv.Itoa(getIntKey(d, "fosid")))
+
+	return resourceObjectEmailfilterBwlRead(d, m)
+}
+
+func resourceObjectEmailfilterBwlDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectEmailfilterBwl(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectEmailfilterBwl resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectEmailfilterBwlRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectEmailfilterBwl(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectEmailfilterBwl resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectEmailfilterBwl(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectEmailfilterBwl resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectEmailfilterBwlComment(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectEmailfilterBwlEntries(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "action"
+		if _, ok := i["action"]; ok {
+			v := flattenObjectEmailfilterBwlEntriesAction(i["action"], d, pre_append)
+			tmp["action"] = fortiAPISubPartPatch(v, "ObjectEmailfilterBwl-Entries-Action")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "addr_type"
+		if _, ok := i["addr-type"]; ok {
+			v := flattenObjectEmailfilterBwlEntriesAddrType(i["addr-type"], d, pre_append)
+			tmp["addr_type"] = fortiAPISubPartPatch(v, "ObjectEmailfilterBwl-Entries-AddrType")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "email_pattern"
+		if _, ok := i["email-pattern"]; ok {
+			v := flattenObjectEmailfilterBwlEntriesEmailPattern(i["email-pattern"], d, pre_append)
+			tmp["email_pattern"] = fortiAPISubPartPatch(v, "ObjectEmailfilterBwl-Entries-EmailPattern")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := i["id"]; ok {
+			v := flattenObjectEmailfilterBwlEntriesId(i["id"], d, pre_append)
+			tmp["id"] = fortiAPISubPartPatch(v, "ObjectEmailfilterBwl-Entries-Id")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "ip4_subnet"
+		if _, ok := i["ip4-subnet"]; ok {
+			v := flattenObjectEmailfilterBwlEntriesIp4Subnet(i["ip4-subnet"], d, pre_append)
+			tmp["ip4_subnet"] = fortiAPISubPartPatch(v, "ObjectEmailfilterBwl-Entries-Ip4Subnet")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "ip6_subnet"
+		if _, ok := i["ip6-subnet"]; ok {
+			v := flattenObjectEmailfilterBwlEntriesIp6Subnet(i["ip6-subnet"], d, pre_append)
+			tmp["ip6_subnet"] = fortiAPISubPartPatch(v, "ObjectEmailfilterBwl-Entries-Ip6Subnet")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "pattern_type"
+		if _, ok := i["pattern-type"]; ok {
+			v := flattenObjectEmailfilterBwlEntriesPatternType(i["pattern-type"], d, pre_append)
+			tmp["pattern_type"] = fortiAPISubPartPatch(v, "ObjectEmailfilterBwl-Entries-PatternType")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "status"
+		if _, ok := i["status"]; ok {
+			v := flattenObjectEmailfilterBwlEntriesStatus(i["status"], d, pre_append)
+			tmp["status"] = fortiAPISubPartPatch(v, "ObjectEmailfilterBwl-Entries-Status")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "type"
+		if _, ok := i["type"]; ok {
+			v := flattenObjectEmailfilterBwlEntriesType(i["type"], d, pre_append)
+			tmp["type"] = fortiAPISubPartPatch(v, "ObjectEmailfilterBwl-Entries-Type")
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenObjectEmailfilterBwlEntriesAction(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			2: "spam",
+			3: "clear",
+			4: "reject",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectEmailfilterBwlEntriesAddrType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "ipv4",
+			1: "ipv6",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectEmailfilterBwlEntriesEmailPattern(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectEmailfilterBwlEntriesId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectEmailfilterBwlEntriesIp4Subnet(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectEmailfilterBwlEntriesIp6Subnet(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectEmailfilterBwlEntriesPatternType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "wildcard",
+			1: "regexp",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectEmailfilterBwlEntriesStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "disable",
+			1: "enable",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectEmailfilterBwlEntriesType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "ip",
+			1: "email",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectEmailfilterBwlId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectEmailfilterBwlName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectObjectEmailfilterBwl(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("comment", flattenObjectEmailfilterBwlComment(o["comment"], d, "comment")); err != nil {
+		if vv, ok := fortiAPIPatch(o["comment"], "ObjectEmailfilterBwl-Comment"); ok {
+			if err = d.Set("comment", vv); err != nil {
+				return fmt.Errorf("Error reading comment: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading comment: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("entries", flattenObjectEmailfilterBwlEntries(o["entries"], d, "entries")); err != nil {
+			if vv, ok := fortiAPIPatch(o["entries"], "ObjectEmailfilterBwl-Entries"); ok {
+				if err = d.Set("entries", vv); err != nil {
+					return fmt.Errorf("Error reading entries: %v", err)
+				}
+			} else {
+				return fmt.Errorf("Error reading entries: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("entries"); ok {
+			if err = d.Set("entries", flattenObjectEmailfilterBwlEntries(o["entries"], d, "entries")); err != nil {
+				if vv, ok := fortiAPIPatch(o["entries"], "ObjectEmailfilterBwl-Entries"); ok {
+					if err = d.Set("entries", vv); err != nil {
+						return fmt.Errorf("Error reading entries: %v", err)
+					}
+				} else {
+					return fmt.Errorf("Error reading entries: %v", err)
+				}
+			}
+		}
+	}
+
+	if err = d.Set("fosid", flattenObjectEmailfilterBwlId(o["id"], d, "fosid")); err != nil {
+		if vv, ok := fortiAPIPatch(o["id"], "ObjectEmailfilterBwl-Id"); ok {
+			if err = d.Set("fosid", vv); err != nil {
+				return fmt.Errorf("Error reading fosid: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenObjectEmailfilterBwlName(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "ObjectEmailfilterBwl-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectEmailfilterBwlFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectEmailfilterBwlComment(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectEmailfilterBwlEntries(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "action"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["action"], _ = expandObjectEmailfilterBwlEntriesAction(d, i["action"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "addr_type"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["addr-type"], _ = expandObjectEmailfilterBwlEntriesAddrType(d, i["addr_type"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "email_pattern"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["email-pattern"], _ = expandObjectEmailfilterBwlEntriesEmailPattern(d, i["email_pattern"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "id"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["id"], _ = expandObjectEmailfilterBwlEntriesId(d, i["id"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "ip4_subnet"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["ip4-subnet"], _ = expandObjectEmailfilterBwlEntriesIp4Subnet(d, i["ip4_subnet"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "ip6_subnet"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["ip6-subnet"], _ = expandObjectEmailfilterBwlEntriesIp6Subnet(d, i["ip6_subnet"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "pattern_type"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["pattern-type"], _ = expandObjectEmailfilterBwlEntriesPatternType(d, i["pattern_type"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "status"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["status"], _ = expandObjectEmailfilterBwlEntriesStatus(d, i["status"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "type"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["type"], _ = expandObjectEmailfilterBwlEntriesType(d, i["type"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandObjectEmailfilterBwlEntriesAction(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectEmailfilterBwlEntriesAddrType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectEmailfilterBwlEntriesEmailPattern(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectEmailfilterBwlEntriesId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectEmailfilterBwlEntriesIp4Subnet(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectEmailfilterBwlEntriesIp6Subnet(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectEmailfilterBwlEntriesPatternType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectEmailfilterBwlEntriesStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectEmailfilterBwlEntriesType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectEmailfilterBwlId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectEmailfilterBwlName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectEmailfilterBwl(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("comment"); ok {
+		t, err := expandObjectEmailfilterBwlComment(d, v, "comment")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["comment"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("entries"); ok {
+		t, err := expandObjectEmailfilterBwlEntries(d, v, "entries")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["entries"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("fosid"); ok {
+		t, err := expandObjectEmailfilterBwlId(d, v, "fosid")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["id"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandObjectEmailfilterBwlName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	return &obj, nil
+}
