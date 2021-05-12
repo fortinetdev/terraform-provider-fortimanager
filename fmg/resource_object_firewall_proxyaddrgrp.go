231d6ed6ca81807a1a97158d87bd99b71ef6e1c6
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:48:09 -0400

    Add resource_object_firewall_proxyaddrgrp

---
+++ fmg/resource_object_firewall_proxyaddrgrp.go
@@ -0,0 +1,577 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Configure web proxy address group.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectFirewallProxyAddrgrp() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectFirewallProxyAddrgrpCreate,
+		Read:   resourceObjectFirewallProxyAddrgrpRead,
+		Update: resourceObjectFirewallProxyAddrgrpUpdate,
+		Delete: resourceObjectFirewallProxyAddrgrpDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"_image_base64": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"color": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"comment": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"member": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"tagging": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"category": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"tags": &schema.Schema{
+							Type:     schema.TypeSet,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"uuid": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"dynamic_sort_subtable": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "false",
+			},
+		},
+	}
+}
+
+func resourceObjectFirewallProxyAddrgrpCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectFirewallProxyAddrgrp(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectFirewallProxyAddrgrp resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectFirewallProxyAddrgrp(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectFirewallProxyAddrgrp resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectFirewallProxyAddrgrpRead(d, m)
+}
+
+func resourceObjectFirewallProxyAddrgrpUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectFirewallProxyAddrgrp(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectFirewallProxyAddrgrp resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectFirewallProxyAddrgrp(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectFirewallProxyAddrgrp resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectFirewallProxyAddrgrpRead(d, m)
+}
+
+func resourceObjectFirewallProxyAddrgrpDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectFirewallProxyAddrgrp(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectFirewallProxyAddrgrp resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectFirewallProxyAddrgrpRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectFirewallProxyAddrgrp(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectFirewallProxyAddrgrp resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectFirewallProxyAddrgrp(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectFirewallProxyAddrgrp resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectFirewallProxyAddrgrpImageBase64(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallProxyAddrgrpColor(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallProxyAddrgrpComment(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallProxyAddrgrpMember(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallProxyAddrgrpName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallProxyAddrgrpTagging(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "category"
+		if _, ok := i["category"]; ok {
+			v := flattenObjectFirewallProxyAddrgrpTaggingCategory(i["category"], d, pre_append)
+			tmp["category"] = fortiAPISubPartPatch(v, "ObjectFirewallProxyAddrgrp-Tagging-Category")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			v := flattenObjectFirewallProxyAddrgrpTaggingName(i["name"], d, pre_append)
+			tmp["name"] = fortiAPISubPartPatch(v, "ObjectFirewallProxyAddrgrp-Tagging-Name")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "tags"
+		if _, ok := i["tags"]; ok {
+			v := flattenObjectFirewallProxyAddrgrpTaggingTags(i["tags"], d, pre_append)
+			tmp["tags"] = fortiAPISubPartPatch(v, "ObjectFirewallProxyAddrgrp-Tagging-Tags")
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenObjectFirewallProxyAddrgrpTaggingCategory(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallProxyAddrgrpTaggingName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallProxyAddrgrpTaggingTags(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return flattenStringList(v)
+}
+
+func flattenObjectFirewallProxyAddrgrpType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "src",
+			1: "dst",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectFirewallProxyAddrgrpUuid(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectObjectFirewallProxyAddrgrp(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("_image_base64", flattenObjectFirewallProxyAddrgrpImageBase64(o["_image-base64"], d, "_image_base64")); err != nil {
+		if vv, ok := fortiAPIPatch(o["_image-base64"], "ObjectFirewallProxyAddrgrp-ImageBase64"); ok {
+			if err = d.Set("_image_base64", vv); err != nil {
+				return fmt.Errorf("Error reading _image_base64: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading _image_base64: %v", err)
+		}
+	}
+
+	if err = d.Set("color", flattenObjectFirewallProxyAddrgrpColor(o["color"], d, "color")); err != nil {
+		if vv, ok := fortiAPIPatch(o["color"], "ObjectFirewallProxyAddrgrp-Color"); ok {
+			if err = d.Set("color", vv); err != nil {
+				return fmt.Errorf("Error reading color: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading color: %v", err)
+		}
+	}
+
+	if err = d.Set("comment", flattenObjectFirewallProxyAddrgrpComment(o["comment"], d, "comment")); err != nil {
+		if vv, ok := fortiAPIPatch(o["comment"], "ObjectFirewallProxyAddrgrp-Comment"); ok {
+			if err = d.Set("comment", vv); err != nil {
+				return fmt.Errorf("Error reading comment: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading comment: %v", err)
+		}
+	}
+
+	if err = d.Set("member", flattenObjectFirewallProxyAddrgrpMember(o["member"], d, "member")); err != nil {
+		if vv, ok := fortiAPIPatch(o["member"], "ObjectFirewallProxyAddrgrp-Member"); ok {
+			if err = d.Set("member", vv); err != nil {
+				return fmt.Errorf("Error reading member: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading member: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenObjectFirewallProxyAddrgrpName(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "ObjectFirewallProxyAddrgrp-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("tagging", flattenObjectFirewallProxyAddrgrpTagging(o["tagging"], d, "tagging")); err != nil {
+			if vv, ok := fortiAPIPatch(o["tagging"], "ObjectFirewallProxyAddrgrp-Tagging"); ok {
+				if err = d.Set("tagging", vv); err != nil {
+					return fmt.Errorf("Error reading tagging: %v", err)
+				}
+			} else {
+				return fmt.Errorf("Error reading tagging: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("tagging"); ok {
+			if err = d.Set("tagging", flattenObjectFirewallProxyAddrgrpTagging(o["tagging"], d, "tagging")); err != nil {
+				if vv, ok := fortiAPIPatch(o["tagging"], "ObjectFirewallProxyAddrgrp-Tagging"); ok {
+					if err = d.Set("tagging", vv); err != nil {
+						return fmt.Errorf("Error reading tagging: %v", err)
+					}
+				} else {
+					return fmt.Errorf("Error reading tagging: %v", err)
+				}
+			}
+		}
+	}
+
+	if err = d.Set("type", flattenObjectFirewallProxyAddrgrpType(o["type"], d, "type")); err != nil {
+		if vv, ok := fortiAPIPatch(o["type"], "ObjectFirewallProxyAddrgrp-Type"); ok {
+			if err = d.Set("type", vv); err != nil {
+				return fmt.Errorf("Error reading type: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading type: %v", err)
+		}
+	}
+
+	if err = d.Set("uuid", flattenObjectFirewallProxyAddrgrpUuid(o["uuid"], d, "uuid")); err != nil {
+		if vv, ok := fortiAPIPatch(o["uuid"], "ObjectFirewallProxyAddrgrp-Uuid"); ok {
+			if err = d.Set("uuid", vv); err != nil {
+				return fmt.Errorf("Error reading uuid: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading uuid: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectFirewallProxyAddrgrpFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectFirewallProxyAddrgrpImageBase64(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallProxyAddrgrpColor(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallProxyAddrgrpComment(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallProxyAddrgrpMember(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallProxyAddrgrpName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallProxyAddrgrpTagging(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "category"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["category"], _ = expandObjectFirewallProxyAddrgrpTaggingCategory(d, i["category"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["name"], _ = expandObjectFirewallProxyAddrgrpTaggingName(d, i["name"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "tags"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["tags"], _ = expandObjectFirewallProxyAddrgrpTaggingTags(d, i["tags"], pre_append)
+		} else {
+			tmp["tags"] = make([]string, 0)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandObjectFirewallProxyAddrgrpTaggingCategory(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallProxyAddrgrpTaggingName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallProxyAddrgrpTaggingTags(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return expandStringList(v.(*schema.Set).List()), nil
+}
+
+func expandObjectFirewallProxyAddrgrpType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallProxyAddrgrpUuid(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectFirewallProxyAddrgrp(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("_image_base64"); ok {
+		t, err := expandObjectFirewallProxyAddrgrpImageBase64(d, v, "_image_base64")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["_image-base64"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("color"); ok {
+		t, err := expandObjectFirewallProxyAddrgrpColor(d, v, "color")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["color"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("comment"); ok {
+		t, err := expandObjectFirewallProxyAddrgrpComment(d, v, "comment")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["comment"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("member"); ok {
+		t, err := expandObjectFirewallProxyAddrgrpMember(d, v, "member")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["member"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandObjectFirewallProxyAddrgrpName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("tagging"); ok {
+		t, err := expandObjectFirewallProxyAddrgrpTagging(d, v, "tagging")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["tagging"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("type"); ok {
+		t, err := expandObjectFirewallProxyAddrgrpType(d, v, "type")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["type"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("uuid"); ok {
+		t, err := expandObjectFirewallProxyAddrgrpUuid(d, v, "uuid")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["uuid"] = t
+		}
+	}
+
+	return &obj, nil
+}
