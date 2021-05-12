98e05a7a536eb6bb902d409fb81a5ad07ccadde3
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:49:41 -0400

    Add resource_object_wirelesscontroller_utmprofile

---
+++ fmg/resource_object_wirelesscontroller_utmprofile.go
@@ -0,0 +1,448 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Configure UTM (Unified Threat Management) profile.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectWirelessControllerUtmProfile() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectWirelessControllerUtmProfileCreate,
+		Read:   resourceObjectWirelessControllerUtmProfileRead,
+		Update: resourceObjectWirelessControllerUtmProfileUpdate,
+		Delete: resourceObjectWirelessControllerUtmProfileDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"antivirus_profile": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"application_list": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"comment": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"ips_sensor": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"scan_botnet_connections": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"utm_log": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"webfilter_profile": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceObjectWirelessControllerUtmProfileCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWirelessControllerUtmProfile(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWirelessControllerUtmProfile resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectWirelessControllerUtmProfile(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWirelessControllerUtmProfile resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectWirelessControllerUtmProfileRead(d, m)
+}
+
+func resourceObjectWirelessControllerUtmProfileUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWirelessControllerUtmProfile(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWirelessControllerUtmProfile resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectWirelessControllerUtmProfile(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWirelessControllerUtmProfile resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectWirelessControllerUtmProfileRead(d, m)
+}
+
+func resourceObjectWirelessControllerUtmProfileDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectWirelessControllerUtmProfile(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectWirelessControllerUtmProfile resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectWirelessControllerUtmProfileRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectWirelessControllerUtmProfile(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWirelessControllerUtmProfile resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectWirelessControllerUtmProfile(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWirelessControllerUtmProfile resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectWirelessControllerUtmProfileAntivirusProfile(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWirelessControllerUtmProfileApplicationList(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWirelessControllerUtmProfileComment(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWirelessControllerUtmProfileIpsSensor(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWirelessControllerUtmProfileName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWirelessControllerUtmProfileScanBotnetConnections(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "disable",
+			1: "block",
+			2: "monitor",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectWirelessControllerUtmProfileUtmLog(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "disable",
+			1: "enable",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectWirelessControllerUtmProfileWebfilterProfile(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectObjectWirelessControllerUtmProfile(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("antivirus_profile", flattenObjectWirelessControllerUtmProfileAntivirusProfile(o["antivirus-profile"], d, "antivirus_profile")); err != nil {
+		if vv, ok := fortiAPIPatch(o["antivirus-profile"], "ObjectWirelessControllerUtmProfile-AntivirusProfile"); ok {
+			if err = d.Set("antivirus_profile", vv); err != nil {
+				return fmt.Errorf("Error reading antivirus_profile: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading antivirus_profile: %v", err)
+		}
+	}
+
+	if err = d.Set("application_list", flattenObjectWirelessControllerUtmProfileApplicationList(o["application-list"], d, "application_list")); err != nil {
+		if vv, ok := fortiAPIPatch(o["application-list"], "ObjectWirelessControllerUtmProfile-ApplicationList"); ok {
+			if err = d.Set("application_list", vv); err != nil {
+				return fmt.Errorf("Error reading application_list: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading application_list: %v", err)
+		}
+	}
+
+	if err = d.Set("comment", flattenObjectWirelessControllerUtmProfileComment(o["comment"], d, "comment")); err != nil {
+		if vv, ok := fortiAPIPatch(o["comment"], "ObjectWirelessControllerUtmProfile-Comment"); ok {
+			if err = d.Set("comment", vv); err != nil {
+				return fmt.Errorf("Error reading comment: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading comment: %v", err)
+		}
+	}
+
+	if err = d.Set("ips_sensor", flattenObjectWirelessControllerUtmProfileIpsSensor(o["ips-sensor"], d, "ips_sensor")); err != nil {
+		if vv, ok := fortiAPIPatch(o["ips-sensor"], "ObjectWirelessControllerUtmProfile-IpsSensor"); ok {
+			if err = d.Set("ips_sensor", vv); err != nil {
+				return fmt.Errorf("Error reading ips_sensor: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading ips_sensor: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenObjectWirelessControllerUtmProfileName(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "ObjectWirelessControllerUtmProfile-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("scan_botnet_connections", flattenObjectWirelessControllerUtmProfileScanBotnetConnections(o["scan-botnet-connections"], d, "scan_botnet_connections")); err != nil {
+		if vv, ok := fortiAPIPatch(o["scan-botnet-connections"], "ObjectWirelessControllerUtmProfile-ScanBotnetConnections"); ok {
+			if err = d.Set("scan_botnet_connections", vv); err != nil {
+				return fmt.Errorf("Error reading scan_botnet_connections: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading scan_botnet_connections: %v", err)
+		}
+	}
+
+	if err = d.Set("utm_log", flattenObjectWirelessControllerUtmProfileUtmLog(o["utm-log"], d, "utm_log")); err != nil {
+		if vv, ok := fortiAPIPatch(o["utm-log"], "ObjectWirelessControllerUtmProfile-UtmLog"); ok {
+			if err = d.Set("utm_log", vv); err != nil {
+				return fmt.Errorf("Error reading utm_log: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading utm_log: %v", err)
+		}
+	}
+
+	if err = d.Set("webfilter_profile", flattenObjectWirelessControllerUtmProfileWebfilterProfile(o["webfilter-profile"], d, "webfilter_profile")); err != nil {
+		if vv, ok := fortiAPIPatch(o["webfilter-profile"], "ObjectWirelessControllerUtmProfile-WebfilterProfile"); ok {
+			if err = d.Set("webfilter_profile", vv); err != nil {
+				return fmt.Errorf("Error reading webfilter_profile: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading webfilter_profile: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectWirelessControllerUtmProfileFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectWirelessControllerUtmProfileAntivirusProfile(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerUtmProfileApplicationList(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerUtmProfileComment(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerUtmProfileIpsSensor(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerUtmProfileName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerUtmProfileScanBotnetConnections(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerUtmProfileUtmLog(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerUtmProfileWebfilterProfile(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectWirelessControllerUtmProfile(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("antivirus_profile"); ok {
+		t, err := expandObjectWirelessControllerUtmProfileAntivirusProfile(d, v, "antivirus_profile")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["antivirus-profile"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("application_list"); ok {
+		t, err := expandObjectWirelessControllerUtmProfileApplicationList(d, v, "application_list")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["application-list"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("comment"); ok {
+		t, err := expandObjectWirelessControllerUtmProfileComment(d, v, "comment")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["comment"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("ips_sensor"); ok {
+		t, err := expandObjectWirelessControllerUtmProfileIpsSensor(d, v, "ips_sensor")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["ips-sensor"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandObjectWirelessControllerUtmProfileName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("scan_botnet_connections"); ok {
+		t, err := expandObjectWirelessControllerUtmProfileScanBotnetConnections(d, v, "scan_botnet_connections")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["scan-botnet-connections"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("utm_log"); ok {
+		t, err := expandObjectWirelessControllerUtmProfileUtmLog(d, v, "utm_log")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["utm-log"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("webfilter_profile"); ok {
+		t, err := expandObjectWirelessControllerUtmProfileWebfilterProfile(d, v, "webfilter_profile")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["webfilter-profile"] = t
+		}
+	}
+
+	return &obj, nil
+}
