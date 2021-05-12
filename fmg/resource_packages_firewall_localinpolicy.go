6ec4a0382da43fb670cae1383df6c228b2dae167
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:49:50 -0400

    Add resource_packages_firewall_localinpolicy

---
+++ fmg/resource_packages_firewall_localinpolicy.go
@@ -0,0 +1,572 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Configure user defined IPv4 local-in policies.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourcePackagesFirewallLocalInPolicy() *schema.Resource {
+	return &schema.Resource{
+		Create: resourcePackagesFirewallLocalInPolicyCreate,
+		Read:   resourcePackagesFirewallLocalInPolicyRead,
+		Update: resourcePackagesFirewallLocalInPolicyUpdate,
+		Delete: resourcePackagesFirewallLocalInPolicyDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"pkg": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"action": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"comments": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"dstaddr": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"ha_mgmt_intf_only": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"intf": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"policyid": &schema.Schema{
+				Type:     schema.TypeInt,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"schedule": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"service": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"srcaddr": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"status": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"uuid": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourcePackagesFirewallLocalInPolicyCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	pkg := d.Get("pkg").(string)
+	var paralist []string
+	paralist = append(paralist, pkg)
+
+	obj, err := getObjectPackagesFirewallLocalInPolicy(d)
+	if err != nil {
+		return fmt.Errorf("Error creating PackagesFirewallLocalInPolicy resource while getting object: %v", err)
+	}
+
+	_, err = c.CreatePackagesFirewallLocalInPolicy(obj, adomv, paralist)
+
+	if err != nil {
+		return fmt.Errorf("Error creating PackagesFirewallLocalInPolicy resource: %v", err)
+	}
+
+	d.SetId(strconv.Itoa(getIntKey(d, "policyid")))
+
+	return resourcePackagesFirewallLocalInPolicyRead(d, m)
+}
+
+func resourcePackagesFirewallLocalInPolicyUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	pkg := d.Get("pkg").(string)
+	var paralist []string
+	paralist = append(paralist, pkg)
+
+	obj, err := getObjectPackagesFirewallLocalInPolicy(d)
+	if err != nil {
+		return fmt.Errorf("Error updating PackagesFirewallLocalInPolicy resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdatePackagesFirewallLocalInPolicy(obj, adomv, mkey, paralist)
+	if err != nil {
+		return fmt.Errorf("Error updating PackagesFirewallLocalInPolicy resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(strconv.Itoa(getIntKey(d, "policyid")))
+
+	return resourcePackagesFirewallLocalInPolicyRead(d, m)
+}
+
+func resourcePackagesFirewallLocalInPolicyDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	pkg := d.Get("pkg").(string)
+	var paralist []string
+	paralist = append(paralist, pkg)
+
+	err = c.DeletePackagesFirewallLocalInPolicy(adomv, mkey, paralist)
+	if err != nil {
+		return fmt.Errorf("Error deleting PackagesFirewallLocalInPolicy resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourcePackagesFirewallLocalInPolicyRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	pkg := d.Get("pkg").(string)
+	var paralist []string
+	paralist = append(paralist, pkg)
+
+	o, err := c.ReadPackagesFirewallLocalInPolicy(adomv, mkey, paralist)
+	if err != nil {
+		return fmt.Errorf("Error reading PackagesFirewallLocalInPolicy resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectPackagesFirewallLocalInPolicy(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading PackagesFirewallLocalInPolicy resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenPackagesFirewallLocalInPolicyAction(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "deny",
+			1: "accept",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenPackagesFirewallLocalInPolicyComments(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenPackagesFirewallLocalInPolicyDstaddr(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenPackagesFirewallLocalInPolicyHaMgmtIntfOnly(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "disable",
+			1: "enable",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenPackagesFirewallLocalInPolicyIntf(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenPackagesFirewallLocalInPolicyPolicyid(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenPackagesFirewallLocalInPolicySchedule(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenPackagesFirewallLocalInPolicyService(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenPackagesFirewallLocalInPolicySrcaddr(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenPackagesFirewallLocalInPolicyStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "disable",
+			1: "enable",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenPackagesFirewallLocalInPolicyUuid(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectPackagesFirewallLocalInPolicy(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("action", flattenPackagesFirewallLocalInPolicyAction(o["action"], d, "action")); err != nil {
+		if vv, ok := fortiAPIPatch(o["action"], "PackagesFirewallLocalInPolicy-Action"); ok {
+			if err = d.Set("action", vv); err != nil {
+				return fmt.Errorf("Error reading action: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading action: %v", err)
+		}
+	}
+
+	if err = d.Set("comments", flattenPackagesFirewallLocalInPolicyComments(o["comments"], d, "comments")); err != nil {
+		if vv, ok := fortiAPIPatch(o["comments"], "PackagesFirewallLocalInPolicy-Comments"); ok {
+			if err = d.Set("comments", vv); err != nil {
+				return fmt.Errorf("Error reading comments: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading comments: %v", err)
+		}
+	}
+
+	if err = d.Set("dstaddr", flattenPackagesFirewallLocalInPolicyDstaddr(o["dstaddr"], d, "dstaddr")); err != nil {
+		if vv, ok := fortiAPIPatch(o["dstaddr"], "PackagesFirewallLocalInPolicy-Dstaddr"); ok {
+			if err = d.Set("dstaddr", vv); err != nil {
+				return fmt.Errorf("Error reading dstaddr: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading dstaddr: %v", err)
+		}
+	}
+
+	if err = d.Set("ha_mgmt_intf_only", flattenPackagesFirewallLocalInPolicyHaMgmtIntfOnly(o["ha-mgmt-intf-only"], d, "ha_mgmt_intf_only")); err != nil {
+		if vv, ok := fortiAPIPatch(o["ha-mgmt-intf-only"], "PackagesFirewallLocalInPolicy-HaMgmtIntfOnly"); ok {
+			if err = d.Set("ha_mgmt_intf_only", vv); err != nil {
+				return fmt.Errorf("Error reading ha_mgmt_intf_only: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading ha_mgmt_intf_only: %v", err)
+		}
+	}
+
+	if err = d.Set("intf", flattenPackagesFirewallLocalInPolicyIntf(o["intf"], d, "intf")); err != nil {
+		if vv, ok := fortiAPIPatch(o["intf"], "PackagesFirewallLocalInPolicy-Intf"); ok {
+			if err = d.Set("intf", vv); err != nil {
+				return fmt.Errorf("Error reading intf: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading intf: %v", err)
+		}
+	}
+
+	if err = d.Set("policyid", flattenPackagesFirewallLocalInPolicyPolicyid(o["policyid"], d, "policyid")); err != nil {
+		if vv, ok := fortiAPIPatch(o["policyid"], "PackagesFirewallLocalInPolicy-Policyid"); ok {
+			if err = d.Set("policyid", vv); err != nil {
+				return fmt.Errorf("Error reading policyid: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading policyid: %v", err)
+		}
+	}
+
+	if err = d.Set("schedule", flattenPackagesFirewallLocalInPolicySchedule(o["schedule"], d, "schedule")); err != nil {
+		if vv, ok := fortiAPIPatch(o["schedule"], "PackagesFirewallLocalInPolicy-Schedule"); ok {
+			if err = d.Set("schedule", vv); err != nil {
+				return fmt.Errorf("Error reading schedule: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading schedule: %v", err)
+		}
+	}
+
+	if err = d.Set("service", flattenPackagesFirewallLocalInPolicyService(o["service"], d, "service")); err != nil {
+		if vv, ok := fortiAPIPatch(o["service"], "PackagesFirewallLocalInPolicy-Service"); ok {
+			if err = d.Set("service", vv); err != nil {
+				return fmt.Errorf("Error reading service: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading service: %v", err)
+		}
+	}
+
+	if err = d.Set("srcaddr", flattenPackagesFirewallLocalInPolicySrcaddr(o["srcaddr"], d, "srcaddr")); err != nil {
+		if vv, ok := fortiAPIPatch(o["srcaddr"], "PackagesFirewallLocalInPolicy-Srcaddr"); ok {
+			if err = d.Set("srcaddr", vv); err != nil {
+				return fmt.Errorf("Error reading srcaddr: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading srcaddr: %v", err)
+		}
+	}
+
+	if err = d.Set("status", flattenPackagesFirewallLocalInPolicyStatus(o["status"], d, "status")); err != nil {
+		if vv, ok := fortiAPIPatch(o["status"], "PackagesFirewallLocalInPolicy-Status"); ok {
+			if err = d.Set("status", vv); err != nil {
+				return fmt.Errorf("Error reading status: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	if err = d.Set("uuid", flattenPackagesFirewallLocalInPolicyUuid(o["uuid"], d, "uuid")); err != nil {
+		if vv, ok := fortiAPIPatch(o["uuid"], "PackagesFirewallLocalInPolicy-Uuid"); ok {
+			if err = d.Set("uuid", vv); err != nil {
+				return fmt.Errorf("Error reading uuid: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading uuid: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenPackagesFirewallLocalInPolicyFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandPackagesFirewallLocalInPolicyAction(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallLocalInPolicyComments(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallLocalInPolicyDstaddr(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallLocalInPolicyHaMgmtIntfOnly(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallLocalInPolicyIntf(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallLocalInPolicyPolicyid(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallLocalInPolicySchedule(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallLocalInPolicyService(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallLocalInPolicySrcaddr(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallLocalInPolicyStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallLocalInPolicyUuid(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectPackagesFirewallLocalInPolicy(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("action"); ok {
+		t, err := expandPackagesFirewallLocalInPolicyAction(d, v, "action")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["action"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("comments"); ok {
+		t, err := expandPackagesFirewallLocalInPolicyComments(d, v, "comments")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["comments"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("dstaddr"); ok {
+		t, err := expandPackagesFirewallLocalInPolicyDstaddr(d, v, "dstaddr")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["dstaddr"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("ha_mgmt_intf_only"); ok {
+		t, err := expandPackagesFirewallLocalInPolicyHaMgmtIntfOnly(d, v, "ha_mgmt_intf_only")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["ha-mgmt-intf-only"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("intf"); ok {
+		t, err := expandPackagesFirewallLocalInPolicyIntf(d, v, "intf")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["intf"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("policyid"); ok {
+		t, err := expandPackagesFirewallLocalInPolicyPolicyid(d, v, "policyid")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["policyid"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("schedule"); ok {
+		t, err := expandPackagesFirewallLocalInPolicySchedule(d, v, "schedule")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["schedule"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("service"); ok {
+		t, err := expandPackagesFirewallLocalInPolicyService(d, v, "service")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["service"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("srcaddr"); ok {
+		t, err := expandPackagesFirewallLocalInPolicySrcaddr(d, v, "srcaddr")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["srcaddr"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("status"); ok {
+		t, err := expandPackagesFirewallLocalInPolicyStatus(d, v, "status")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["status"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("uuid"); ok {
+		t, err := expandPackagesFirewallLocalInPolicyUuid(d, v, "uuid")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["uuid"] = t
+		}
+	}
+
+	return &obj, nil
+}
