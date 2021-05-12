caade6a319e8e72335fcb5ab5180c17c1b3160c3
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:49:15 -0400

    Add resource_object_user_vcenter

---
+++ fmg/resource_object_user_vcenter.go
@@ -0,0 +1,519 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: ObjectUser Vcenter
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectUserVcenter() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectUserVcenterCreate,
+		Read:   resourceObjectUserVcenterRead,
+		Update: resourceObjectUserVcenterUpdate,
+		Delete: resourceObjectUserVcenterDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"password": &schema.Schema{
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+				Computed: true,
+			},
+			"rule": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"rule": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"server": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"status": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"upd_interval": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"user": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"dynamic_sort_subtable": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "false",
+			},
+		},
+	}
+}
+
+func resourceObjectUserVcenterCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectUserVcenter(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectUserVcenter resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectUserVcenter(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectUserVcenter resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectUserVcenterRead(d, m)
+}
+
+func resourceObjectUserVcenterUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectUserVcenter(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectUserVcenter resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectUserVcenter(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectUserVcenter resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectUserVcenterRead(d, m)
+}
+
+func resourceObjectUserVcenterDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectUserVcenter(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectUserVcenter resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectUserVcenterRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectUserVcenter(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectUserVcenter resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectUserVcenter(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectUserVcenter resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectUserVcenterName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectUserVcenterPassword(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return flattenStringList(v)
+}
+
+func flattenObjectUserVcenterRule(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			v := flattenObjectUserVcenterRuleName(i["name"], d, pre_append)
+			tmp["name"] = fortiAPISubPartPatch(v, "ObjectUserVcenter-Rule-Name")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "rule"
+		if _, ok := i["rule"]; ok {
+			v := flattenObjectUserVcenterRuleRule(i["rule"], d, pre_append)
+			tmp["rule"] = fortiAPISubPartPatch(v, "ObjectUserVcenter-Rule-Rule")
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenObjectUserVcenterRuleName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectUserVcenterRuleRule(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectUserVcenterServer(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectUserVcenterStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "disable",
+			1: "enable",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectUserVcenterUpdInterval(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectUserVcenterUser(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectObjectUserVcenter(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", flattenObjectUserVcenterName(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "ObjectUserVcenter-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("password", flattenObjectUserVcenterPassword(o["password"], d, "password")); err != nil {
+		if vv, ok := fortiAPIPatch(o["password"], "ObjectUserVcenter-Password"); ok {
+			if err = d.Set("password", vv); err != nil {
+				return fmt.Errorf("Error reading password: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading password: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("rule", flattenObjectUserVcenterRule(o["rule"], d, "rule")); err != nil {
+			if vv, ok := fortiAPIPatch(o["rule"], "ObjectUserVcenter-Rule"); ok {
+				if err = d.Set("rule", vv); err != nil {
+					return fmt.Errorf("Error reading rule: %v", err)
+				}
+			} else {
+				return fmt.Errorf("Error reading rule: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("rule"); ok {
+			if err = d.Set("rule", flattenObjectUserVcenterRule(o["rule"], d, "rule")); err != nil {
+				if vv, ok := fortiAPIPatch(o["rule"], "ObjectUserVcenter-Rule"); ok {
+					if err = d.Set("rule", vv); err != nil {
+						return fmt.Errorf("Error reading rule: %v", err)
+					}
+				} else {
+					return fmt.Errorf("Error reading rule: %v", err)
+				}
+			}
+		}
+	}
+
+	if err = d.Set("server", flattenObjectUserVcenterServer(o["server"], d, "server")); err != nil {
+		if vv, ok := fortiAPIPatch(o["server"], "ObjectUserVcenter-Server"); ok {
+			if err = d.Set("server", vv); err != nil {
+				return fmt.Errorf("Error reading server: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading server: %v", err)
+		}
+	}
+
+	if err = d.Set("status", flattenObjectUserVcenterStatus(o["status"], d, "status")); err != nil {
+		if vv, ok := fortiAPIPatch(o["status"], "ObjectUserVcenter-Status"); ok {
+			if err = d.Set("status", vv); err != nil {
+				return fmt.Errorf("Error reading status: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	if err = d.Set("upd_interval", flattenObjectUserVcenterUpdInterval(o["upd_interval"], d, "upd_interval")); err != nil {
+		if vv, ok := fortiAPIPatch(o["upd_interval"], "ObjectUserVcenter-UpdInterval"); ok {
+			if err = d.Set("upd_interval", vv); err != nil {
+				return fmt.Errorf("Error reading upd_interval: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading upd_interval: %v", err)
+		}
+	}
+
+	if err = d.Set("user", flattenObjectUserVcenterUser(o["user"], d, "user")); err != nil {
+		if vv, ok := fortiAPIPatch(o["user"], "ObjectUserVcenter-User"); ok {
+			if err = d.Set("user", vv); err != nil {
+				return fmt.Errorf("Error reading user: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading user: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectUserVcenterFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectUserVcenterName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectUserVcenterPassword(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return expandStringList(v.(*schema.Set).List()), nil
+}
+
+func expandObjectUserVcenterRule(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["name"], _ = expandObjectUserVcenterRuleName(d, i["name"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "rule"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["rule"], _ = expandObjectUserVcenterRuleRule(d, i["rule"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandObjectUserVcenterRuleName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectUserVcenterRuleRule(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectUserVcenterServer(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectUserVcenterStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectUserVcenterUpdInterval(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectUserVcenterUser(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectUserVcenter(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandObjectUserVcenterName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("password"); ok {
+		t, err := expandObjectUserVcenterPassword(d, v, "password")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["password"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("rule"); ok {
+		t, err := expandObjectUserVcenterRule(d, v, "rule")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["rule"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("server"); ok {
+		t, err := expandObjectUserVcenterServer(d, v, "server")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["server"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("status"); ok {
+		t, err := expandObjectUserVcenterStatus(d, v, "status")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["status"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("upd_interval"); ok {
+		t, err := expandObjectUserVcenterUpdInterval(d, v, "upd_interval")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["upd_interval"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("user"); ok {
+		t, err := expandObjectUserVcenterUser(d, v, "user")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["user"] = t
+		}
+	}
+
+	return &obj, nil
+}
