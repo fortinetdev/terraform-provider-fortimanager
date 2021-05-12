e3b09f86c0a4558fb68c9258713021ebfedce9e0
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:47:52 -0400

    Add resource_object_dynamic_vip

---
+++ fmg/resource_object_dynamic_vip.go
@@ -0,0 +1,239 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: ObjectDynamic Vip
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectDynamicVip() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectDynamicVipCreate,
+		Read:   resourceObjectDynamicVipRead,
+		Update: resourceObjectDynamicVipUpdate,
+		Delete: resourceObjectDynamicVipDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"description": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceObjectDynamicVipCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectDynamicVip(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectDynamicVip resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectDynamicVip(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectDynamicVip resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectDynamicVipRead(d, m)
+}
+
+func resourceObjectDynamicVipUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectDynamicVip(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectDynamicVip resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectDynamicVip(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectDynamicVip resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectDynamicVipRead(d, m)
+}
+
+func resourceObjectDynamicVipDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectDynamicVip(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectDynamicVip resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectDynamicVipRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectDynamicVip(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectDynamicVip resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectDynamicVip(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectDynamicVip resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectDynamicVipDescription(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectDynamicVipName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectObjectDynamicVip(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("description", flattenObjectDynamicVipDescription(o["description"], d, "description")); err != nil {
+		if vv, ok := fortiAPIPatch(o["description"], "ObjectDynamicVip-Description"); ok {
+			if err = d.Set("description", vv); err != nil {
+				return fmt.Errorf("Error reading description: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading description: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenObjectDynamicVipName(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "ObjectDynamicVip-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectDynamicVipFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectDynamicVipDescription(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectDynamicVipName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectDynamicVip(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("description"); ok {
+		t, err := expandObjectDynamicVipDescription(d, v, "description")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["description"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandObjectDynamicVipName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	return &obj, nil
+}
