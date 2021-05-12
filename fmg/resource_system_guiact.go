c73eacf24239b9495e991fd9650537a3e3c0fc09
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:50:23 -0400

    Add resource_system_guiact

---
+++ fmg/resource_system_guiact.go
@@ -0,0 +1,408 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: System settings through GUI.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSystemGuiact() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSystemGuiactUpdate,
+		Read:   resourceSystemGuiactRead,
+		Update: resourceSystemGuiactUpdate,
+		Delete: resourceSystemGuiactDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"backup_all": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"backup_conf": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"eventlog_msg": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"eventlog_path": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"reboot": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"reset2default": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"restore_all": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"restore_conf": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"time": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceSystemGuiactUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	obj, err := getObjectSystemGuiact(d)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemGuiact resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateSystemGuiact(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemGuiact resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId("SystemGuiact")
+
+	return resourceSystemGuiactRead(d, m)
+}
+
+func resourceSystemGuiactDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	err = c.DeleteSystemGuiact(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting SystemGuiact resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSystemGuiactRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	o, err := c.ReadSystemGuiact(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemGuiact resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectSystemGuiact(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemGuiact resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenSystemGuiactBackupAll(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemGuiactBackupConf(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemGuiactEventlogMsg(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemGuiactEventlogPath(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemGuiactReboot(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemGuiactReset2Default(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemGuiactRestoreAll(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemGuiactRestoreConf(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemGuiactTime(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectSystemGuiact(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("backup_all", flattenSystemGuiactBackupAll(o["backup_all"], d, "backup_all")); err != nil {
+		if vv, ok := fortiAPIPatch(o["backup_all"], "SystemGuiact-BackupAll"); ok {
+			if err = d.Set("backup_all", vv); err != nil {
+				return fmt.Errorf("Error reading backup_all: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading backup_all: %v", err)
+		}
+	}
+
+	if err = d.Set("backup_conf", flattenSystemGuiactBackupConf(o["backup_conf"], d, "backup_conf")); err != nil {
+		if vv, ok := fortiAPIPatch(o["backup_conf"], "SystemGuiact-BackupConf"); ok {
+			if err = d.Set("backup_conf", vv); err != nil {
+				return fmt.Errorf("Error reading backup_conf: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading backup_conf: %v", err)
+		}
+	}
+
+	if err = d.Set("eventlog_msg", flattenSystemGuiactEventlogMsg(o["eventlog_msg"], d, "eventlog_msg")); err != nil {
+		if vv, ok := fortiAPIPatch(o["eventlog_msg"], "SystemGuiact-EventlogMsg"); ok {
+			if err = d.Set("eventlog_msg", vv); err != nil {
+				return fmt.Errorf("Error reading eventlog_msg: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading eventlog_msg: %v", err)
+		}
+	}
+
+	if err = d.Set("eventlog_path", flattenSystemGuiactEventlogPath(o["eventlog_path"], d, "eventlog_path")); err != nil {
+		if vv, ok := fortiAPIPatch(o["eventlog_path"], "SystemGuiact-EventlogPath"); ok {
+			if err = d.Set("eventlog_path", vv); err != nil {
+				return fmt.Errorf("Error reading eventlog_path: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading eventlog_path: %v", err)
+		}
+	}
+
+	if err = d.Set("reboot", flattenSystemGuiactReboot(o["reboot"], d, "reboot")); err != nil {
+		if vv, ok := fortiAPIPatch(o["reboot"], "SystemGuiact-Reboot"); ok {
+			if err = d.Set("reboot", vv); err != nil {
+				return fmt.Errorf("Error reading reboot: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading reboot: %v", err)
+		}
+	}
+
+	if err = d.Set("reset2default", flattenSystemGuiactReset2Default(o["reset2default"], d, "reset2default")); err != nil {
+		if vv, ok := fortiAPIPatch(o["reset2default"], "SystemGuiact-Reset2Default"); ok {
+			if err = d.Set("reset2default", vv); err != nil {
+				return fmt.Errorf("Error reading reset2default: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading reset2default: %v", err)
+		}
+	}
+
+	if err = d.Set("restore_all", flattenSystemGuiactRestoreAll(o["restore_all"], d, "restore_all")); err != nil {
+		if vv, ok := fortiAPIPatch(o["restore_all"], "SystemGuiact-RestoreAll"); ok {
+			if err = d.Set("restore_all", vv); err != nil {
+				return fmt.Errorf("Error reading restore_all: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading restore_all: %v", err)
+		}
+	}
+
+	if err = d.Set("restore_conf", flattenSystemGuiactRestoreConf(o["restore_conf"], d, "restore_conf")); err != nil {
+		if vv, ok := fortiAPIPatch(o["restore_conf"], "SystemGuiact-RestoreConf"); ok {
+			if err = d.Set("restore_conf", vv); err != nil {
+				return fmt.Errorf("Error reading restore_conf: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading restore_conf: %v", err)
+		}
+	}
+
+	if err = d.Set("time", flattenSystemGuiactTime(o["time"], d, "time")); err != nil {
+		if vv, ok := fortiAPIPatch(o["time"], "SystemGuiact-Time"); ok {
+			if err = d.Set("time", vv); err != nil {
+				return fmt.Errorf("Error reading time: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading time: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenSystemGuiactFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandSystemGuiactBackupAll(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemGuiactBackupConf(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemGuiactEventlogMsg(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemGuiactEventlogPath(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemGuiactReboot(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemGuiactReset2Default(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemGuiactRestoreAll(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemGuiactRestoreConf(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemGuiactTime(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectSystemGuiact(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("backup_all"); ok {
+		t, err := expandSystemGuiactBackupAll(d, v, "backup_all")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["backup_all"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("backup_conf"); ok {
+		t, err := expandSystemGuiactBackupConf(d, v, "backup_conf")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["backup_conf"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("eventlog_msg"); ok {
+		t, err := expandSystemGuiactEventlogMsg(d, v, "eventlog_msg")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["eventlog_msg"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("eventlog_path"); ok {
+		t, err := expandSystemGuiactEventlogPath(d, v, "eventlog_path")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["eventlog_path"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("reboot"); ok {
+		t, err := expandSystemGuiactReboot(d, v, "reboot")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["reboot"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("reset2default"); ok {
+		t, err := expandSystemGuiactReset2Default(d, v, "reset2default")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["reset2default"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("restore_all"); ok {
+		t, err := expandSystemGuiactRestoreAll(d, v, "restore_all")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["restore_all"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("restore_conf"); ok {
+		t, err := expandSystemGuiactRestoreConf(d, v, "restore_conf")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["restore_conf"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("time"); ok {
+		t, err := expandSystemGuiactTime(d, v, "time")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["time"] = t
+		}
+	}
+
+	return &obj, nil
+}
