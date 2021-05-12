4349001d828032cbae2d97f73ddc06e1097c2e63
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:49:42 -0400

    Add resource_object_wirelesscontroller_vapgroup

---
+++ fmg/resource_object_wirelesscontroller_vapgroup.go
@@ -0,0 +1,271 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Configure virtual Access Point (VAP) groups.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectWirelessControllerVapGroup() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectWirelessControllerVapGroupCreate,
+		Read:   resourceObjectWirelessControllerVapGroupRead,
+		Update: resourceObjectWirelessControllerVapGroupUpdate,
+		Delete: resourceObjectWirelessControllerVapGroupDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"comment": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"vaps": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceObjectWirelessControllerVapGroupCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWirelessControllerVapGroup(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWirelessControllerVapGroup resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectWirelessControllerVapGroup(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWirelessControllerVapGroup resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectWirelessControllerVapGroupRead(d, m)
+}
+
+func resourceObjectWirelessControllerVapGroupUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWirelessControllerVapGroup(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWirelessControllerVapGroup resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectWirelessControllerVapGroup(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWirelessControllerVapGroup resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectWirelessControllerVapGroupRead(d, m)
+}
+
+func resourceObjectWirelessControllerVapGroupDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectWirelessControllerVapGroup(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectWirelessControllerVapGroup resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectWirelessControllerVapGroupRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectWirelessControllerVapGroup(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWirelessControllerVapGroup resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectWirelessControllerVapGroup(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWirelessControllerVapGroup resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectWirelessControllerVapGroupComment(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWirelessControllerVapGroupName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWirelessControllerVapGroupVaps(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectObjectWirelessControllerVapGroup(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("comment", flattenObjectWirelessControllerVapGroupComment(o["comment"], d, "comment")); err != nil {
+		if vv, ok := fortiAPIPatch(o["comment"], "ObjectWirelessControllerVapGroup-Comment"); ok {
+			if err = d.Set("comment", vv); err != nil {
+				return fmt.Errorf("Error reading comment: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading comment: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenObjectWirelessControllerVapGroupName(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "ObjectWirelessControllerVapGroup-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("vaps", flattenObjectWirelessControllerVapGroupVaps(o["vaps"], d, "vaps")); err != nil {
+		if vv, ok := fortiAPIPatch(o["vaps"], "ObjectWirelessControllerVapGroup-Vaps"); ok {
+			if err = d.Set("vaps", vv); err != nil {
+				return fmt.Errorf("Error reading vaps: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading vaps: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectWirelessControllerVapGroupFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectWirelessControllerVapGroupComment(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerVapGroupName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerVapGroupVaps(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectWirelessControllerVapGroup(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("comment"); ok {
+		t, err := expandObjectWirelessControllerVapGroupComment(d, v, "comment")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["comment"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandObjectWirelessControllerVapGroupName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("vaps"); ok {
+		t, err := expandObjectWirelessControllerVapGroupVaps(d, v, "vaps")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["vaps"] = t
+		}
+	}
+
+	return &obj, nil
+}
