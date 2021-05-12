e095ebd44694864d867d85e829c3d3cb935586d2
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:50:50 -0400

    Add resource_system_log_maildomain

---
+++ fmg/resource_system_log_maildomain.go
@@ -0,0 +1,270 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: FortiMail domain setting.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSystemLogMailDomain() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSystemLogMailDomainCreate,
+		Read:   resourceSystemLogMailDomainRead,
+		Update: resourceSystemLogMailDomainUpdate,
+		Delete: resourceSystemLogMailDomainDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"devices": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"domain": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"fosid": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"vdom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceSystemLogMailDomainCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	obj, err := getObjectSystemLogMailDomain(d)
+	if err != nil {
+		return fmt.Errorf("Error creating SystemLogMailDomain resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateSystemLogMailDomain(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating SystemLogMailDomain resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, ""))
+
+	return resourceSystemLogMailDomainRead(d, m)
+}
+
+func resourceSystemLogMailDomainUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	obj, err := getObjectSystemLogMailDomain(d)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemLogMailDomain resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateSystemLogMailDomain(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemLogMailDomain resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, ""))
+
+	return resourceSystemLogMailDomainRead(d, m)
+}
+
+func resourceSystemLogMailDomainDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	err = c.DeleteSystemLogMailDomain(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting SystemLogMailDomain resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSystemLogMailDomainRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	o, err := c.ReadSystemLogMailDomain(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemLogMailDomain resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectSystemLogMailDomain(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemLogMailDomain resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenSystemLogMailDomainDevices(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemLogMailDomainDomain(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemLogMailDomainId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemLogMailDomainVdom(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectSystemLogMailDomain(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("devices", flattenSystemLogMailDomainDevices(o["devices"], d, "devices")); err != nil {
+		if vv, ok := fortiAPIPatch(o["devices"], "SystemLogMailDomain-Devices"); ok {
+			if err = d.Set("devices", vv); err != nil {
+				return fmt.Errorf("Error reading devices: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading devices: %v", err)
+		}
+	}
+
+	if err = d.Set("domain", flattenSystemLogMailDomainDomain(o["domain"], d, "domain")); err != nil {
+		if vv, ok := fortiAPIPatch(o["domain"], "SystemLogMailDomain-Domain"); ok {
+			if err = d.Set("domain", vv); err != nil {
+				return fmt.Errorf("Error reading domain: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading domain: %v", err)
+		}
+	}
+
+	if err = d.Set("fosid", flattenSystemLogMailDomainId(o["id"], d, "fosid")); err != nil {
+		if vv, ok := fortiAPIPatch(o["id"], "SystemLogMailDomain-Id"); ok {
+			if err = d.Set("fosid", vv); err != nil {
+				return fmt.Errorf("Error reading fosid: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	if err = d.Set("vdom", flattenSystemLogMailDomainVdom(o["vdom"], d, "vdom")); err != nil {
+		if vv, ok := fortiAPIPatch(o["vdom"], "SystemLogMailDomain-Vdom"); ok {
+			if err = d.Set("vdom", vv); err != nil {
+				return fmt.Errorf("Error reading vdom: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading vdom: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenSystemLogMailDomainFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandSystemLogMailDomainDevices(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemLogMailDomainDomain(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemLogMailDomainId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemLogMailDomainVdom(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectSystemLogMailDomain(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("devices"); ok {
+		t, err := expandSystemLogMailDomainDevices(d, v, "devices")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["devices"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("domain"); ok {
+		t, err := expandSystemLogMailDomainDomain(d, v, "domain")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["domain"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("fosid"); ok {
+		t, err := expandSystemLogMailDomainId(d, v, "fosid")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["id"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("vdom"); ok {
+		t, err := expandSystemLogMailDomainVdom(d, v, "vdom")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["vdom"] = t
+		}
+	}
+
+	return &obj, nil
+}
