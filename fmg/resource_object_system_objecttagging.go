487c2b3212e9ad42c3961b25ab6c4282c08f3233
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:49:01 -0400

    Add resource_object_system_objecttagging

---
+++ fmg/resource_object_system_objecttagging.go
@@ -0,0 +1,435 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Configure object tagging.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectSystemObjectTagging() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectSystemObjectTaggingCreate,
+		Read:   resourceObjectSystemObjectTaggingRead,
+		Update: resourceObjectSystemObjectTaggingUpdate,
+		Delete: resourceObjectSystemObjectTaggingDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"address": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"category": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"color": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"device": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"interface": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"multiple": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"tags": &schema.Schema{
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceObjectSystemObjectTaggingCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectSystemObjectTagging(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectSystemObjectTagging resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectSystemObjectTagging(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectSystemObjectTagging resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, "category"))
+
+	return resourceObjectSystemObjectTaggingRead(d, m)
+}
+
+func resourceObjectSystemObjectTaggingUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectSystemObjectTagging(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectSystemObjectTagging resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectSystemObjectTagging(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectSystemObjectTagging resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, "category"))
+
+	return resourceObjectSystemObjectTaggingRead(d, m)
+}
+
+func resourceObjectSystemObjectTaggingDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectSystemObjectTagging(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectSystemObjectTagging resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectSystemObjectTaggingRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectSystemObjectTagging(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectSystemObjectTagging resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectSystemObjectTagging(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectSystemObjectTagging resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectSystemObjectTaggingAddress(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "optional",
+			1: "mandatory",
+			2: "disable",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectSystemObjectTaggingCategory(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectSystemObjectTaggingColor(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectSystemObjectTaggingDevice(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "optional",
+			1: "mandatory",
+			2: "disable",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectSystemObjectTaggingInterface(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "optional",
+			1: "mandatory",
+			2: "disable",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectSystemObjectTaggingMultiple(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "disable",
+			1: "enable",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectSystemObjectTaggingTags(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return flattenStringList(v)
+}
+
+func refreshObjectObjectSystemObjectTagging(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("address", flattenObjectSystemObjectTaggingAddress(o["address"], d, "address")); err != nil {
+		if vv, ok := fortiAPIPatch(o["address"], "ObjectSystemObjectTagging-Address"); ok {
+			if err = d.Set("address", vv); err != nil {
+				return fmt.Errorf("Error reading address: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading address: %v", err)
+		}
+	}
+
+	if err = d.Set("category", flattenObjectSystemObjectTaggingCategory(o["category"], d, "category")); err != nil {
+		if vv, ok := fortiAPIPatch(o["category"], "ObjectSystemObjectTagging-Category"); ok {
+			if err = d.Set("category", vv); err != nil {
+				return fmt.Errorf("Error reading category: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading category: %v", err)
+		}
+	}
+
+	if err = d.Set("color", flattenObjectSystemObjectTaggingColor(o["color"], d, "color")); err != nil {
+		if vv, ok := fortiAPIPatch(o["color"], "ObjectSystemObjectTagging-Color"); ok {
+			if err = d.Set("color", vv); err != nil {
+				return fmt.Errorf("Error reading color: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading color: %v", err)
+		}
+	}
+
+	if err = d.Set("device", flattenObjectSystemObjectTaggingDevice(o["device"], d, "device")); err != nil {
+		if vv, ok := fortiAPIPatch(o["device"], "ObjectSystemObjectTagging-Device"); ok {
+			if err = d.Set("device", vv); err != nil {
+				return fmt.Errorf("Error reading device: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading device: %v", err)
+		}
+	}
+
+	if err = d.Set("interface", flattenObjectSystemObjectTaggingInterface(o["interface"], d, "interface")); err != nil {
+		if vv, ok := fortiAPIPatch(o["interface"], "ObjectSystemObjectTagging-Interface"); ok {
+			if err = d.Set("interface", vv); err != nil {
+				return fmt.Errorf("Error reading interface: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading interface: %v", err)
+		}
+	}
+
+	if err = d.Set("multiple", flattenObjectSystemObjectTaggingMultiple(o["multiple"], d, "multiple")); err != nil {
+		if vv, ok := fortiAPIPatch(o["multiple"], "ObjectSystemObjectTagging-Multiple"); ok {
+			if err = d.Set("multiple", vv); err != nil {
+				return fmt.Errorf("Error reading multiple: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading multiple: %v", err)
+		}
+	}
+
+	if err = d.Set("tags", flattenObjectSystemObjectTaggingTags(o["tags"], d, "tags")); err != nil {
+		if vv, ok := fortiAPIPatch(o["tags"], "ObjectSystemObjectTagging-Tags"); ok {
+			if err = d.Set("tags", vv); err != nil {
+				return fmt.Errorf("Error reading tags: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading tags: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectSystemObjectTaggingFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectSystemObjectTaggingAddress(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectSystemObjectTaggingCategory(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectSystemObjectTaggingColor(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectSystemObjectTaggingDevice(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectSystemObjectTaggingInterface(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectSystemObjectTaggingMultiple(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectSystemObjectTaggingTags(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return expandStringList(v.(*schema.Set).List()), nil
+}
+
+func getObjectObjectSystemObjectTagging(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("address"); ok {
+		t, err := expandObjectSystemObjectTaggingAddress(d, v, "address")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["address"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("category"); ok {
+		t, err := expandObjectSystemObjectTaggingCategory(d, v, "category")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["category"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("color"); ok {
+		t, err := expandObjectSystemObjectTaggingColor(d, v, "color")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["color"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("device"); ok {
+		t, err := expandObjectSystemObjectTaggingDevice(d, v, "device")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["device"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("interface"); ok {
+		t, err := expandObjectSystemObjectTaggingInterface(d, v, "interface")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["interface"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("multiple"); ok {
+		t, err := expandObjectSystemObjectTaggingMultiple(d, v, "multiple")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["multiple"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("tags"); ok {
+		t, err := expandObjectSystemObjectTaggingTags(d, v, "tags")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["tags"] = t
+		}
+	}
+
+	return &obj, nil
+}
