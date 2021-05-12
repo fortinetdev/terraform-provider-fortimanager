9cacf73fd6e54d1927ba919e34525b5527dae3ee
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:49:52 -0400

    Add resource_packages_firewall_multicastpolicy6

---
+++ fmg/resource_packages_firewall_multicastpolicy6.go
@@ -0,0 +1,644 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Configure IPv6 multicast NAT policies.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourcePackagesFirewallMulticastPolicy6() *schema.Resource {
+	return &schema.Resource{
+		Create: resourcePackagesFirewallMulticastPolicy6Create,
+		Read:   resourcePackagesFirewallMulticastPolicy6Read,
+		Update: resourcePackagesFirewallMulticastPolicy6Update,
+		Delete: resourcePackagesFirewallMulticastPolicy6Delete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"pkg": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"action": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"auto_asic_offload": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"dstaddr": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"dstintf": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"end_port": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"fosid": &schema.Schema{
+				Type:     schema.TypeInt,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"logtraffic": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"protocol": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"srcaddr": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"srcintf": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"start_port": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"status": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourcePackagesFirewallMulticastPolicy6Create(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	pkg := d.Get("pkg").(string)
+	var paralist []string
+	paralist = append(paralist, pkg)
+
+	obj, err := getObjectPackagesFirewallMulticastPolicy6(d)
+	if err != nil {
+		return fmt.Errorf("Error creating PackagesFirewallMulticastPolicy6 resource while getting object: %v", err)
+	}
+
+	_, err = c.CreatePackagesFirewallMulticastPolicy6(obj, adomv, paralist)
+
+	if err != nil {
+		return fmt.Errorf("Error creating PackagesFirewallMulticastPolicy6 resource: %v", err)
+	}
+
+	d.SetId(strconv.Itoa(getIntKey(d, "fosid")))
+
+	return resourcePackagesFirewallMulticastPolicy6Read(d, m)
+}
+
+func resourcePackagesFirewallMulticastPolicy6Update(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	pkg := d.Get("pkg").(string)
+	var paralist []string
+	paralist = append(paralist, pkg)
+
+	obj, err := getObjectPackagesFirewallMulticastPolicy6(d)
+	if err != nil {
+		return fmt.Errorf("Error updating PackagesFirewallMulticastPolicy6 resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdatePackagesFirewallMulticastPolicy6(obj, adomv, mkey, paralist)
+	if err != nil {
+		return fmt.Errorf("Error updating PackagesFirewallMulticastPolicy6 resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(strconv.Itoa(getIntKey(d, "fosid")))
+
+	return resourcePackagesFirewallMulticastPolicy6Read(d, m)
+}
+
+func resourcePackagesFirewallMulticastPolicy6Delete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	pkg := d.Get("pkg").(string)
+	var paralist []string
+	paralist = append(paralist, pkg)
+
+	err = c.DeletePackagesFirewallMulticastPolicy6(adomv, mkey, paralist)
+	if err != nil {
+		return fmt.Errorf("Error deleting PackagesFirewallMulticastPolicy6 resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourcePackagesFirewallMulticastPolicy6Read(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	pkg := d.Get("pkg").(string)
+	var paralist []string
+	paralist = append(paralist, pkg)
+
+	o, err := c.ReadPackagesFirewallMulticastPolicy6(adomv, mkey, paralist)
+	if err != nil {
+		return fmt.Errorf("Error reading PackagesFirewallMulticastPolicy6 resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectPackagesFirewallMulticastPolicy6(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading PackagesFirewallMulticastPolicy6 resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenPackagesFirewallMulticastPolicy6Action(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "deny",
+			1: "accept",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenPackagesFirewallMulticastPolicy6AutoAsicOffload(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "disable",
+			1: "enable",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenPackagesFirewallMulticastPolicy6Dstaddr(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenPackagesFirewallMulticastPolicy6Dstintf(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenPackagesFirewallMulticastPolicy6EndPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenPackagesFirewallMulticastPolicy6Id(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenPackagesFirewallMulticastPolicy6Logtraffic(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "disable",
+			1: "enable",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenPackagesFirewallMulticastPolicy6Name(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenPackagesFirewallMulticastPolicy6Protocol(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenPackagesFirewallMulticastPolicy6Srcaddr(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenPackagesFirewallMulticastPolicy6Srcintf(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenPackagesFirewallMulticastPolicy6StartPort(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenPackagesFirewallMulticastPolicy6Status(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "disable",
+			1: "enable",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func refreshObjectPackagesFirewallMulticastPolicy6(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("action", flattenPackagesFirewallMulticastPolicy6Action(o["action"], d, "action")); err != nil {
+		if vv, ok := fortiAPIPatch(o["action"], "PackagesFirewallMulticastPolicy6-Action"); ok {
+			if err = d.Set("action", vv); err != nil {
+				return fmt.Errorf("Error reading action: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading action: %v", err)
+		}
+	}
+
+	if err = d.Set("auto_asic_offload", flattenPackagesFirewallMulticastPolicy6AutoAsicOffload(o["auto-asic-offload"], d, "auto_asic_offload")); err != nil {
+		if vv, ok := fortiAPIPatch(o["auto-asic-offload"], "PackagesFirewallMulticastPolicy6-AutoAsicOffload"); ok {
+			if err = d.Set("auto_asic_offload", vv); err != nil {
+				return fmt.Errorf("Error reading auto_asic_offload: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading auto_asic_offload: %v", err)
+		}
+	}
+
+	if err = d.Set("dstaddr", flattenPackagesFirewallMulticastPolicy6Dstaddr(o["dstaddr"], d, "dstaddr")); err != nil {
+		if vv, ok := fortiAPIPatch(o["dstaddr"], "PackagesFirewallMulticastPolicy6-Dstaddr"); ok {
+			if err = d.Set("dstaddr", vv); err != nil {
+				return fmt.Errorf("Error reading dstaddr: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading dstaddr: %v", err)
+		}
+	}
+
+	if err = d.Set("dstintf", flattenPackagesFirewallMulticastPolicy6Dstintf(o["dstintf"], d, "dstintf")); err != nil {
+		if vv, ok := fortiAPIPatch(o["dstintf"], "PackagesFirewallMulticastPolicy6-Dstintf"); ok {
+			if err = d.Set("dstintf", vv); err != nil {
+				return fmt.Errorf("Error reading dstintf: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading dstintf: %v", err)
+		}
+	}
+
+	if err = d.Set("end_port", flattenPackagesFirewallMulticastPolicy6EndPort(o["end-port"], d, "end_port")); err != nil {
+		if vv, ok := fortiAPIPatch(o["end-port"], "PackagesFirewallMulticastPolicy6-EndPort"); ok {
+			if err = d.Set("end_port", vv); err != nil {
+				return fmt.Errorf("Error reading end_port: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading end_port: %v", err)
+		}
+	}
+
+	if err = d.Set("fosid", flattenPackagesFirewallMulticastPolicy6Id(o["id"], d, "fosid")); err != nil {
+		if vv, ok := fortiAPIPatch(o["id"], "PackagesFirewallMulticastPolicy6-Id"); ok {
+			if err = d.Set("fosid", vv); err != nil {
+				return fmt.Errorf("Error reading fosid: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	if err = d.Set("logtraffic", flattenPackagesFirewallMulticastPolicy6Logtraffic(o["logtraffic"], d, "logtraffic")); err != nil {
+		if vv, ok := fortiAPIPatch(o["logtraffic"], "PackagesFirewallMulticastPolicy6-Logtraffic"); ok {
+			if err = d.Set("logtraffic", vv); err != nil {
+				return fmt.Errorf("Error reading logtraffic: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading logtraffic: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenPackagesFirewallMulticastPolicy6Name(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "PackagesFirewallMulticastPolicy6-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("protocol", flattenPackagesFirewallMulticastPolicy6Protocol(o["protocol"], d, "protocol")); err != nil {
+		if vv, ok := fortiAPIPatch(o["protocol"], "PackagesFirewallMulticastPolicy6-Protocol"); ok {
+			if err = d.Set("protocol", vv); err != nil {
+				return fmt.Errorf("Error reading protocol: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading protocol: %v", err)
+		}
+	}
+
+	if err = d.Set("srcaddr", flattenPackagesFirewallMulticastPolicy6Srcaddr(o["srcaddr"], d, "srcaddr")); err != nil {
+		if vv, ok := fortiAPIPatch(o["srcaddr"], "PackagesFirewallMulticastPolicy6-Srcaddr"); ok {
+			if err = d.Set("srcaddr", vv); err != nil {
+				return fmt.Errorf("Error reading srcaddr: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading srcaddr: %v", err)
+		}
+	}
+
+	if err = d.Set("srcintf", flattenPackagesFirewallMulticastPolicy6Srcintf(o["srcintf"], d, "srcintf")); err != nil {
+		if vv, ok := fortiAPIPatch(o["srcintf"], "PackagesFirewallMulticastPolicy6-Srcintf"); ok {
+			if err = d.Set("srcintf", vv); err != nil {
+				return fmt.Errorf("Error reading srcintf: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading srcintf: %v", err)
+		}
+	}
+
+	if err = d.Set("start_port", flattenPackagesFirewallMulticastPolicy6StartPort(o["start-port"], d, "start_port")); err != nil {
+		if vv, ok := fortiAPIPatch(o["start-port"], "PackagesFirewallMulticastPolicy6-StartPort"); ok {
+			if err = d.Set("start_port", vv); err != nil {
+				return fmt.Errorf("Error reading start_port: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading start_port: %v", err)
+		}
+	}
+
+	if err = d.Set("status", flattenPackagesFirewallMulticastPolicy6Status(o["status"], d, "status")); err != nil {
+		if vv, ok := fortiAPIPatch(o["status"], "PackagesFirewallMulticastPolicy6-Status"); ok {
+			if err = d.Set("status", vv); err != nil {
+				return fmt.Errorf("Error reading status: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenPackagesFirewallMulticastPolicy6FortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandPackagesFirewallMulticastPolicy6Action(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallMulticastPolicy6AutoAsicOffload(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallMulticastPolicy6Dstaddr(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallMulticastPolicy6Dstintf(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallMulticastPolicy6EndPort(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallMulticastPolicy6Id(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallMulticastPolicy6Logtraffic(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallMulticastPolicy6Name(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallMulticastPolicy6Protocol(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallMulticastPolicy6Srcaddr(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallMulticastPolicy6Srcintf(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallMulticastPolicy6StartPort(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandPackagesFirewallMulticastPolicy6Status(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectPackagesFirewallMulticastPolicy6(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("action"); ok {
+		t, err := expandPackagesFirewallMulticastPolicy6Action(d, v, "action")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["action"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("auto_asic_offload"); ok {
+		t, err := expandPackagesFirewallMulticastPolicy6AutoAsicOffload(d, v, "auto_asic_offload")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["auto-asic-offload"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("dstaddr"); ok {
+		t, err := expandPackagesFirewallMulticastPolicy6Dstaddr(d, v, "dstaddr")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["dstaddr"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("dstintf"); ok {
+		t, err := expandPackagesFirewallMulticastPolicy6Dstintf(d, v, "dstintf")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["dstintf"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("end_port"); ok {
+		t, err := expandPackagesFirewallMulticastPolicy6EndPort(d, v, "end_port")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["end-port"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("fosid"); ok {
+		t, err := expandPackagesFirewallMulticastPolicy6Id(d, v, "fosid")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["id"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("logtraffic"); ok {
+		t, err := expandPackagesFirewallMulticastPolicy6Logtraffic(d, v, "logtraffic")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["logtraffic"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandPackagesFirewallMulticastPolicy6Name(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("protocol"); ok {
+		t, err := expandPackagesFirewallMulticastPolicy6Protocol(d, v, "protocol")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["protocol"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("srcaddr"); ok {
+		t, err := expandPackagesFirewallMulticastPolicy6Srcaddr(d, v, "srcaddr")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["srcaddr"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("srcintf"); ok {
+		t, err := expandPackagesFirewallMulticastPolicy6Srcintf(d, v, "srcintf")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["srcintf"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("start_port"); ok {
+		t, err := expandPackagesFirewallMulticastPolicy6StartPort(d, v, "start_port")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["start-port"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("status"); ok {
+		t, err := expandPackagesFirewallMulticastPolicy6Status(d, v, "status")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["status"] = t
+		}
+	}
+
+	return &obj, nil
+}
