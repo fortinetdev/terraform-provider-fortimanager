454b5ddc9f497da78e580f0c3565ff6fd58d03b7
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:50:43 -0400

    Add resource_system_locallog_fortianalyzer3_setting

---
+++ fmg/resource_system_locallog_fortianalyzer3_setting.go
@@ -0,0 +1,352 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Settings for locallog to fortianalyzer.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSystemLocallogFortianalyzer3Setting() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSystemLocallogFortianalyzer3SettingUpdate,
+		Read:   resourceSystemLocallogFortianalyzer3SettingRead,
+		Update: resourceSystemLocallogFortianalyzer3SettingUpdate,
+		Delete: resourceSystemLocallogFortianalyzer3SettingDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"reliable": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"secure_connection": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"server": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"severity": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"status": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"upload_time": &schema.Schema{
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceSystemLocallogFortianalyzer3SettingUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	obj, err := getObjectSystemLocallogFortianalyzer3Setting(d)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemLocallogFortianalyzer3Setting resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateSystemLocallogFortianalyzer3Setting(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemLocallogFortianalyzer3Setting resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId("SystemLocallogFortianalyzer3Setting")
+
+	return resourceSystemLocallogFortianalyzer3SettingRead(d, m)
+}
+
+func resourceSystemLocallogFortianalyzer3SettingDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	err = c.DeleteSystemLocallogFortianalyzer3Setting(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting SystemLocallogFortianalyzer3Setting resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSystemLocallogFortianalyzer3SettingRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	o, err := c.ReadSystemLocallogFortianalyzer3Setting(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemLocallogFortianalyzer3Setting resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectSystemLocallogFortianalyzer3Setting(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemLocallogFortianalyzer3Setting resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenSystemLocallogFortianalyzer3SettingReliable(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "disable",
+			1: "enable",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenSystemLocallogFortianalyzer3SettingSecureConnection(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "disable",
+			1: "enable",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenSystemLocallogFortianalyzer3SettingServer(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemLocallogFortianalyzer3SettingSeverity(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "emergency",
+			1: "alert",
+			2: "critical",
+			3: "error",
+			4: "warning",
+			5: "notification",
+			6: "information",
+			7: "debug",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenSystemLocallogFortianalyzer3SettingStatus(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "disable",
+			1: "realtime",
+			2: "upload",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenSystemLocallogFortianalyzer3SettingUploadTime(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return flattenStringList(v)
+}
+
+func refreshObjectSystemLocallogFortianalyzer3Setting(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("reliable", flattenSystemLocallogFortianalyzer3SettingReliable(o["reliable"], d, "reliable")); err != nil {
+		if vv, ok := fortiAPIPatch(o["reliable"], "SystemLocallogFortianalyzer3Setting-Reliable"); ok {
+			if err = d.Set("reliable", vv); err != nil {
+				return fmt.Errorf("Error reading reliable: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading reliable: %v", err)
+		}
+	}
+
+	if err = d.Set("secure_connection", flattenSystemLocallogFortianalyzer3SettingSecureConnection(o["secure-connection"], d, "secure_connection")); err != nil {
+		if vv, ok := fortiAPIPatch(o["secure-connection"], "SystemLocallogFortianalyzer3Setting-SecureConnection"); ok {
+			if err = d.Set("secure_connection", vv); err != nil {
+				return fmt.Errorf("Error reading secure_connection: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading secure_connection: %v", err)
+		}
+	}
+
+	if err = d.Set("server", flattenSystemLocallogFortianalyzer3SettingServer(o["server"], d, "server")); err != nil {
+		if vv, ok := fortiAPIPatch(o["server"], "SystemLocallogFortianalyzer3Setting-Server"); ok {
+			if err = d.Set("server", vv); err != nil {
+				return fmt.Errorf("Error reading server: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading server: %v", err)
+		}
+	}
+
+	if err = d.Set("severity", flattenSystemLocallogFortianalyzer3SettingSeverity(o["severity"], d, "severity")); err != nil {
+		if vv, ok := fortiAPIPatch(o["severity"], "SystemLocallogFortianalyzer3Setting-Severity"); ok {
+			if err = d.Set("severity", vv); err != nil {
+				return fmt.Errorf("Error reading severity: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading severity: %v", err)
+		}
+	}
+
+	if err = d.Set("status", flattenSystemLocallogFortianalyzer3SettingStatus(o["status"], d, "status")); err != nil {
+		if vv, ok := fortiAPIPatch(o["status"], "SystemLocallogFortianalyzer3Setting-Status"); ok {
+			if err = d.Set("status", vv); err != nil {
+				return fmt.Errorf("Error reading status: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading status: %v", err)
+		}
+	}
+
+	if err = d.Set("upload_time", flattenSystemLocallogFortianalyzer3SettingUploadTime(o["upload-time"], d, "upload_time")); err != nil {
+		if vv, ok := fortiAPIPatch(o["upload-time"], "SystemLocallogFortianalyzer3Setting-UploadTime"); ok {
+			if err = d.Set("upload_time", vv); err != nil {
+				return fmt.Errorf("Error reading upload_time: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading upload_time: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenSystemLocallogFortianalyzer3SettingFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandSystemLocallogFortianalyzer3SettingReliable(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemLocallogFortianalyzer3SettingSecureConnection(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemLocallogFortianalyzer3SettingServer(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemLocallogFortianalyzer3SettingSeverity(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemLocallogFortianalyzer3SettingStatus(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemLocallogFortianalyzer3SettingUploadTime(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return expandStringList(v.(*schema.Set).List()), nil
+}
+
+func getObjectSystemLocallogFortianalyzer3Setting(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("reliable"); ok {
+		t, err := expandSystemLocallogFortianalyzer3SettingReliable(d, v, "reliable")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["reliable"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("secure_connection"); ok {
+		t, err := expandSystemLocallogFortianalyzer3SettingSecureConnection(d, v, "secure_connection")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["secure-connection"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("server"); ok {
+		t, err := expandSystemLocallogFortianalyzer3SettingServer(d, v, "server")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["server"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("severity"); ok {
+		t, err := expandSystemLocallogFortianalyzer3SettingSeverity(d, v, "severity")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["severity"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("status"); ok {
+		t, err := expandSystemLocallogFortianalyzer3SettingStatus(d, v, "status")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["status"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("upload_time"); ok {
+		t, err := expandSystemLocallogFortianalyzer3SettingUploadTime(d, v, "upload_time")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["upload-time"] = t
+		}
+	}
+
+	return &obj, nil
+}
