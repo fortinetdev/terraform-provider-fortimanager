456ba73a06af7218ea7035f64a0ea37e36431586
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:50:17 -0400

    Add resource_system_certificate_oftp

---
+++ fmg/resource_system_certificate_oftp.go
@@ -0,0 +1,324 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: OFTP certificates and keys.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSystemCertificateOftp() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSystemCertificateOftpUpdate,
+		Read:   resourceSystemCertificateOftpRead,
+		Update: resourceSystemCertificateOftpUpdate,
+		Delete: resourceSystemCertificateOftpDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"certificate": &schema.Schema{
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+				Computed: true,
+			},
+			"comment": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"local": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"mode": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"password": &schema.Schema{
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+				Computed: true,
+			},
+			"private_key": &schema.Schema{
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceSystemCertificateOftpUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	obj, err := getObjectSystemCertificateOftp(d)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemCertificateOftp resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateSystemCertificateOftp(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemCertificateOftp resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId("SystemCertificateOftp")
+
+	return resourceSystemCertificateOftpRead(d, m)
+}
+
+func resourceSystemCertificateOftpDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	err = c.DeleteSystemCertificateOftp(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting SystemCertificateOftp resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSystemCertificateOftpRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	o, err := c.ReadSystemCertificateOftp(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemCertificateOftp resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectSystemCertificateOftp(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemCertificateOftp resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenSystemCertificateOftpCertificate(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return flattenStringList(v)
+}
+
+func flattenSystemCertificateOftpComment(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemCertificateOftpLocal(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemCertificateOftpMode(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "default",
+			1: "custom",
+			2: "local",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenSystemCertificateOftpPassword(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return flattenStringList(v)
+}
+
+func flattenSystemCertificateOftpPrivateKey(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return flattenStringList(v)
+}
+
+func refreshObjectSystemCertificateOftp(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("certificate", flattenSystemCertificateOftpCertificate(o["certificate"], d, "certificate")); err != nil {
+		if vv, ok := fortiAPIPatch(o["certificate"], "SystemCertificateOftp-Certificate"); ok {
+			if err = d.Set("certificate", vv); err != nil {
+				return fmt.Errorf("Error reading certificate: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading certificate: %v", err)
+		}
+	}
+
+	if err = d.Set("comment", flattenSystemCertificateOftpComment(o["comment"], d, "comment")); err != nil {
+		if vv, ok := fortiAPIPatch(o["comment"], "SystemCertificateOftp-Comment"); ok {
+			if err = d.Set("comment", vv); err != nil {
+				return fmt.Errorf("Error reading comment: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading comment: %v", err)
+		}
+	}
+
+	if err = d.Set("local", flattenSystemCertificateOftpLocal(o["local"], d, "local")); err != nil {
+		if vv, ok := fortiAPIPatch(o["local"], "SystemCertificateOftp-Local"); ok {
+			if err = d.Set("local", vv); err != nil {
+				return fmt.Errorf("Error reading local: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading local: %v", err)
+		}
+	}
+
+	if err = d.Set("mode", flattenSystemCertificateOftpMode(o["mode"], d, "mode")); err != nil {
+		if vv, ok := fortiAPIPatch(o["mode"], "SystemCertificateOftp-Mode"); ok {
+			if err = d.Set("mode", vv); err != nil {
+				return fmt.Errorf("Error reading mode: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading mode: %v", err)
+		}
+	}
+
+	if err = d.Set("password", flattenSystemCertificateOftpPassword(o["password"], d, "password")); err != nil {
+		if vv, ok := fortiAPIPatch(o["password"], "SystemCertificateOftp-Password"); ok {
+			if err = d.Set("password", vv); err != nil {
+				return fmt.Errorf("Error reading password: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading password: %v", err)
+		}
+	}
+
+	if err = d.Set("private_key", flattenSystemCertificateOftpPrivateKey(o["private-key"], d, "private_key")); err != nil {
+		if vv, ok := fortiAPIPatch(o["private-key"], "SystemCertificateOftp-PrivateKey"); ok {
+			if err = d.Set("private_key", vv); err != nil {
+				return fmt.Errorf("Error reading private_key: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading private_key: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenSystemCertificateOftpFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandSystemCertificateOftpCertificate(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return expandStringList(v.(*schema.Set).List()), nil
+}
+
+func expandSystemCertificateOftpComment(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemCertificateOftpLocal(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemCertificateOftpMode(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemCertificateOftpPassword(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return expandStringList(v.(*schema.Set).List()), nil
+}
+
+func expandSystemCertificateOftpPrivateKey(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return expandStringList(v.(*schema.Set).List()), nil
+}
+
+func getObjectSystemCertificateOftp(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("certificate"); ok {
+		t, err := expandSystemCertificateOftpCertificate(d, v, "certificate")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["certificate"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("comment"); ok {
+		t, err := expandSystemCertificateOftpComment(d, v, "comment")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["comment"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("local"); ok {
+		t, err := expandSystemCertificateOftpLocal(d, v, "local")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["local"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("mode"); ok {
+		t, err := expandSystemCertificateOftpMode(d, v, "mode")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["mode"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("password"); ok {
+		t, err := expandSystemCertificateOftpPassword(d, v, "password")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["password"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("private_key"); ok {
+		t, err := expandSystemCertificateOftpPrivateKey(d, v, "private_key")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["private-key"] = t
+		}
+	}
+
+	return &obj, nil
+}
