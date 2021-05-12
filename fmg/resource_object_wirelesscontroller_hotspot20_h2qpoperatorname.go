6de93582299219a384c496148497d9ea542d8eb2
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:49:37 -0400

    Add resource_object_wirelesscontroller_hotspot20_h2qpoperatorname

---
+++ fmg/resource_object_wirelesscontroller_hotspot20_h2qpoperatorname.go
@@ -0,0 +1,374 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Configure operator friendly name.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectWirelessControllerHotspot20H2QpOperatorName() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectWirelessControllerHotspot20H2QpOperatorNameCreate,
+		Read:   resourceObjectWirelessControllerHotspot20H2QpOperatorNameRead,
+		Update: resourceObjectWirelessControllerHotspot20H2QpOperatorNameUpdate,
+		Delete: resourceObjectWirelessControllerHotspot20H2QpOperatorNameDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"value_list": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"index": &schema.Schema{
+							Type:     schema.TypeInt,
+							Optional: true,
+							Computed: true,
+						},
+						"lang": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"value": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"dynamic_sort_subtable": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "false",
+			},
+		},
+	}
+}
+
+func resourceObjectWirelessControllerHotspot20H2QpOperatorNameCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWirelessControllerHotspot20H2QpOperatorName(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWirelessControllerHotspot20H2QpOperatorName resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectWirelessControllerHotspot20H2QpOperatorName(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWirelessControllerHotspot20H2QpOperatorName resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectWirelessControllerHotspot20H2QpOperatorNameRead(d, m)
+}
+
+func resourceObjectWirelessControllerHotspot20H2QpOperatorNameUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWirelessControllerHotspot20H2QpOperatorName(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWirelessControllerHotspot20H2QpOperatorName resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectWirelessControllerHotspot20H2QpOperatorName(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWirelessControllerHotspot20H2QpOperatorName resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectWirelessControllerHotspot20H2QpOperatorNameRead(d, m)
+}
+
+func resourceObjectWirelessControllerHotspot20H2QpOperatorNameDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectWirelessControllerHotspot20H2QpOperatorName(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectWirelessControllerHotspot20H2QpOperatorName resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectWirelessControllerHotspot20H2QpOperatorNameRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectWirelessControllerHotspot20H2QpOperatorName(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWirelessControllerHotspot20H2QpOperatorName resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectWirelessControllerHotspot20H2QpOperatorName(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWirelessControllerHotspot20H2QpOperatorName resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectWirelessControllerHotspot20H2QpOperatorNameName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWirelessControllerHotspot20H2QpOperatorNameValueList(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "index"
+		if _, ok := i["index"]; ok {
+			v := flattenObjectWirelessControllerHotspot20H2QpOperatorNameValueListIndex(i["index"], d, pre_append)
+			tmp["index"] = fortiAPISubPartPatch(v, "ObjectWirelessControllerHotspot20H2QpOperatorName-ValueList-Index")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "lang"
+		if _, ok := i["lang"]; ok {
+			v := flattenObjectWirelessControllerHotspot20H2QpOperatorNameValueListLang(i["lang"], d, pre_append)
+			tmp["lang"] = fortiAPISubPartPatch(v, "ObjectWirelessControllerHotspot20H2QpOperatorName-ValueList-Lang")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "value"
+		if _, ok := i["value"]; ok {
+			v := flattenObjectWirelessControllerHotspot20H2QpOperatorNameValueListValue(i["value"], d, pre_append)
+			tmp["value"] = fortiAPISubPartPatch(v, "ObjectWirelessControllerHotspot20H2QpOperatorName-ValueList-Value")
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenObjectWirelessControllerHotspot20H2QpOperatorNameValueListIndex(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWirelessControllerHotspot20H2QpOperatorNameValueListLang(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWirelessControllerHotspot20H2QpOperatorNameValueListValue(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectObjectWirelessControllerHotspot20H2QpOperatorName(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", flattenObjectWirelessControllerHotspot20H2QpOperatorNameName(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "ObjectWirelessControllerHotspot20H2QpOperatorName-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("value_list", flattenObjectWirelessControllerHotspot20H2QpOperatorNameValueList(o["value-list"], d, "value_list")); err != nil {
+			if vv, ok := fortiAPIPatch(o["value-list"], "ObjectWirelessControllerHotspot20H2QpOperatorName-ValueList"); ok {
+				if err = d.Set("value_list", vv); err != nil {
+					return fmt.Errorf("Error reading value_list: %v", err)
+				}
+			} else {
+				return fmt.Errorf("Error reading value_list: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("value_list"); ok {
+			if err = d.Set("value_list", flattenObjectWirelessControllerHotspot20H2QpOperatorNameValueList(o["value-list"], d, "value_list")); err != nil {
+				if vv, ok := fortiAPIPatch(o["value-list"], "ObjectWirelessControllerHotspot20H2QpOperatorName-ValueList"); ok {
+					if err = d.Set("value_list", vv); err != nil {
+						return fmt.Errorf("Error reading value_list: %v", err)
+					}
+				} else {
+					return fmt.Errorf("Error reading value_list: %v", err)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectWirelessControllerHotspot20H2QpOperatorNameFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectWirelessControllerHotspot20H2QpOperatorNameName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerHotspot20H2QpOperatorNameValueList(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "index"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["index"], _ = expandObjectWirelessControllerHotspot20H2QpOperatorNameValueListIndex(d, i["index"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "lang"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["lang"], _ = expandObjectWirelessControllerHotspot20H2QpOperatorNameValueListLang(d, i["lang"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "value"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["value"], _ = expandObjectWirelessControllerHotspot20H2QpOperatorNameValueListValue(d, i["value"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandObjectWirelessControllerHotspot20H2QpOperatorNameValueListIndex(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerHotspot20H2QpOperatorNameValueListLang(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerHotspot20H2QpOperatorNameValueListValue(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectWirelessControllerHotspot20H2QpOperatorName(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandObjectWirelessControllerHotspot20H2QpOperatorNameName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("value_list"); ok {
+		t, err := expandObjectWirelessControllerHotspot20H2QpOperatorNameValueList(d, v, "value_list")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["value-list"] = t
+		}
+	}
+
+	return &obj, nil
+}
