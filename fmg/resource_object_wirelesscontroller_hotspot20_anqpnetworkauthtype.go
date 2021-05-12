f2c5a2fe10ca692735be5657233c334be1b1b52e
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:49:35 -0400

    Add resource_object_wirelesscontroller_hotspot20_anqpnetworkauthtype

---
+++ fmg/resource_object_wirelesscontroller_hotspot20_anqpnetworkauthtype.go
@@ -0,0 +1,281 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Configure network authentication type.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectWirelessControllerHotspot20AnqpNetworkAuthType() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectWirelessControllerHotspot20AnqpNetworkAuthTypeCreate,
+		Read:   resourceObjectWirelessControllerHotspot20AnqpNetworkAuthTypeRead,
+		Update: resourceObjectWirelessControllerHotspot20AnqpNetworkAuthTypeUpdate,
+		Delete: resourceObjectWirelessControllerHotspot20AnqpNetworkAuthTypeDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"auth_type": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"url": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceObjectWirelessControllerHotspot20AnqpNetworkAuthTypeCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWirelessControllerHotspot20AnqpNetworkAuthType(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWirelessControllerHotspot20AnqpNetworkAuthType resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectWirelessControllerHotspot20AnqpNetworkAuthType(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWirelessControllerHotspot20AnqpNetworkAuthType resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectWirelessControllerHotspot20AnqpNetworkAuthTypeRead(d, m)
+}
+
+func resourceObjectWirelessControllerHotspot20AnqpNetworkAuthTypeUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWirelessControllerHotspot20AnqpNetworkAuthType(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWirelessControllerHotspot20AnqpNetworkAuthType resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectWirelessControllerHotspot20AnqpNetworkAuthType(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWirelessControllerHotspot20AnqpNetworkAuthType resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectWirelessControllerHotspot20AnqpNetworkAuthTypeRead(d, m)
+}
+
+func resourceObjectWirelessControllerHotspot20AnqpNetworkAuthTypeDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectWirelessControllerHotspot20AnqpNetworkAuthType(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectWirelessControllerHotspot20AnqpNetworkAuthType resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectWirelessControllerHotspot20AnqpNetworkAuthTypeRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectWirelessControllerHotspot20AnqpNetworkAuthType(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWirelessControllerHotspot20AnqpNetworkAuthType resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectWirelessControllerHotspot20AnqpNetworkAuthType(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWirelessControllerHotspot20AnqpNetworkAuthType resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectWirelessControllerHotspot20AnqpNetworkAuthTypeAuthType(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0: "acceptance-of-terms",
+			1: "online-enrollment",
+			2: "http-redirection",
+			3: "dns-redirection",
+		}
+		res := getEnumVal(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenObjectWirelessControllerHotspot20AnqpNetworkAuthTypeName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWirelessControllerHotspot20AnqpNetworkAuthTypeUrl(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectObjectWirelessControllerHotspot20AnqpNetworkAuthType(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("auth_type", flattenObjectWirelessControllerHotspot20AnqpNetworkAuthTypeAuthType(o["auth-type"], d, "auth_type")); err != nil {
+		if vv, ok := fortiAPIPatch(o["auth-type"], "ObjectWirelessControllerHotspot20AnqpNetworkAuthType-AuthType"); ok {
+			if err = d.Set("auth_type", vv); err != nil {
+				return fmt.Errorf("Error reading auth_type: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading auth_type: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenObjectWirelessControllerHotspot20AnqpNetworkAuthTypeName(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "ObjectWirelessControllerHotspot20AnqpNetworkAuthType-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("url", flattenObjectWirelessControllerHotspot20AnqpNetworkAuthTypeUrl(o["url"], d, "url")); err != nil {
+		if vv, ok := fortiAPIPatch(o["url"], "ObjectWirelessControllerHotspot20AnqpNetworkAuthType-Url"); ok {
+			if err = d.Set("url", vv); err != nil {
+				return fmt.Errorf("Error reading url: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading url: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectWirelessControllerHotspot20AnqpNetworkAuthTypeFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectWirelessControllerHotspot20AnqpNetworkAuthTypeAuthType(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerHotspot20AnqpNetworkAuthTypeName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerHotspot20AnqpNetworkAuthTypeUrl(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectWirelessControllerHotspot20AnqpNetworkAuthType(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("auth_type"); ok {
+		t, err := expandObjectWirelessControllerHotspot20AnqpNetworkAuthTypeAuthType(d, v, "auth_type")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["auth-type"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandObjectWirelessControllerHotspot20AnqpNetworkAuthTypeName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("url"); ok {
+		t, err := expandObjectWirelessControllerHotspot20AnqpNetworkAuthTypeUrl(d, v, "url")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["url"] = t
+		}
+	}
+
+	return &obj, nil
+}
