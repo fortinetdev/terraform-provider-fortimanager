6c8a8e577265c5bb73c1f7be29c6fb2f0b50b305
Frank Shen <fshen01@fortinet.com>
2021-05-12 10:44:44 -0400

    Fix sdk version error

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/terraform-providers/terraform-provider-fortimanager
 go 1.13
 
 require (
-	github.com/fortinetdev/forti-sdk-go2 v1.1.16
+	github.com/fortinetdev/forti-sdk-go v1.2.1
 	github.com/google/uuid v1.1.1
 	github.com/hashicorp/terraform-plugin-sdk v1.12.0
 )
--- vendor/github.com/fortinetdev/forti-sdk-go/fortimanager2/sdkcore/exec_workspace_action.go
+++ vendor/github.com/fortinetdev/forti-sdk-go/fortimanager2/sdkcore/exec_workspace_action.go
@@ -1,6 +1,6 @@
 package forticlient
 
-// CreateUpdateFirewallSecurityPolicySort API operation for FortiOS to sort the firewall policies.
+// CreateUpdateExecWorkspaceAction API operation for locking ADOMs, devices, or policy packages so that an administrator can prevent other administrators from making changes to the elements that they are working in.
 // Returns error for service API and SDK errors.
 func (c *FortiSDKClient) CreateUpdateExecWorkspaceAction(globaladom, action, target, param string) (output map[string]interface{}, err error) {
 	path := "/dvmdb/[*]/workspace"
@@ -20,28 +20,3 @@ func (c *FortiSDKClient) CreateUpdateExecWorkspaceAction(globaladom, action, tar
 	err = createUpdate(c, globaladom, path, "exec", nil, output, false)
 	return
 }
-
-
-// // ReadFirewallSecurityPolicySort API operation for FortiOS to read the firewall policies sort results
-// // Returns sort status
-// // Returns error for service API and SDK errors.
-// func (c *FortiSDKClient) ReadFirewallSecurityPolicySort(sortby, sortdirection string) (sorted bool, err error) {
-// 	idlist, err := getPolicyList(c)
-// 	if err != nil {
-// 		err = fmt.Errorf("sort err %s", err)
-// 		return
-// 	}
-
-// 	bsorted := bPolicyListSorted(idlist, sortby, sortdirection)
-// 	log.Printf("shengh: %v", bsorted)
-// 	if bsorted == true {
-// 		sorted = true
-// 		return
-// 	}
-
-// 	sorted = false
-
-// 	return
-// }
-
-
