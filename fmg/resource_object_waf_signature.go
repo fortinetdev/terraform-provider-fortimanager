a86538ea4e7d60251d30dd16936062dfdb730f93
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:49:21 -0400

    Add resource_object_waf_signature

---
+++ fmg/resource_object_waf_signature.go
@@ -0,0 +1,239 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Hidden table for datasource.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectWafSignature() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectWafSignatureCreate,
+		Read:   resourceObjectWafSignatureRead,
+		Update: resourceObjectWafSignatureUpdate,
+		Delete: resourceObjectWafSignatureDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"desc": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"fosid": &schema.Schema{
+				Type:     schema.TypeInt,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceObjectWafSignatureCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWafSignature(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWafSignature resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectWafSignature(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWafSignature resource: %v", err)
+	}
+
+	d.SetId(strconv.Itoa(getIntKey(d, "fosid")))
+
+	return resourceObjectWafSignatureRead(d, m)
+}
+
+func resourceObjectWafSignatureUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWafSignature(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWafSignature resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectWafSignature(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWafSignature resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(strconv.Itoa(getIntKey(d, "fosid")))
+
+	return resourceObjectWafSignatureRead(d, m)
+}
+
+func resourceObjectWafSignatureDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectWafSignature(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectWafSignature resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectWafSignatureRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectWafSignature(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWafSignature resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectWafSignature(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWafSignature resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectWafSignatureDesc(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWafSignatureId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectObjectWafSignature(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("desc", flattenObjectWafSignatureDesc(o["desc"], d, "desc")); err != nil {
+		if vv, ok := fortiAPIPatch(o["desc"], "ObjectWafSignature-Desc"); ok {
+			if err = d.Set("desc", vv); err != nil {
+				return fmt.Errorf("Error reading desc: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading desc: %v", err)
+		}
+	}
+
+	if err = d.Set("fosid", flattenObjectWafSignatureId(o["id"], d, "fosid")); err != nil {
+		if vv, ok := fortiAPIPatch(o["id"], "ObjectWafSignature-Id"); ok {
+			if err = d.Set("fosid", vv); err != nil {
+				return fmt.Errorf("Error reading fosid: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading fosid: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectWafSignatureFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectWafSignatureDesc(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWafSignatureId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectWafSignature(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("desc"); ok {
+		t, err := expandObjectWafSignatureDesc(d, v, "desc")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["desc"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("fosid"); ok {
+		t, err := expandObjectWafSignatureId(d, v, "fosid")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["id"] = t
+		}
+	}
+
+	return &obj, nil
+}
