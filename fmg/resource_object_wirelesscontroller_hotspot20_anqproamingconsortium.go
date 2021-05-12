46bcfff2c717f954605869ce6532554fc5720e39
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:49:35 -0400

    Add resource_object_wirelesscontroller_hotspot20_anqproamingconsortium

---
+++ fmg/resource_object_wirelesscontroller_hotspot20_anqproamingconsortium.go
@@ -0,0 +1,374 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Configure roaming consortium.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectWirelessControllerHotspot20AnqpRoamingConsortium() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectWirelessControllerHotspot20AnqpRoamingConsortiumCreate,
+		Read:   resourceObjectWirelessControllerHotspot20AnqpRoamingConsortiumRead,
+		Update: resourceObjectWirelessControllerHotspot20AnqpRoamingConsortiumUpdate,
+		Delete: resourceObjectWirelessControllerHotspot20AnqpRoamingConsortiumDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"oi_list": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"comment": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"index": &schema.Schema{
+							Type:     schema.TypeInt,
+							Optional: true,
+							Computed: true,
+						},
+						"oi": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"dynamic_sort_subtable": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "false",
+			},
+		},
+	}
+}
+
+func resourceObjectWirelessControllerHotspot20AnqpRoamingConsortiumCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWirelessControllerHotspot20AnqpRoamingConsortium(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWirelessControllerHotspot20AnqpRoamingConsortium resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectWirelessControllerHotspot20AnqpRoamingConsortium(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectWirelessControllerHotspot20AnqpRoamingConsortium resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectWirelessControllerHotspot20AnqpRoamingConsortiumRead(d, m)
+}
+
+func resourceObjectWirelessControllerHotspot20AnqpRoamingConsortiumUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectWirelessControllerHotspot20AnqpRoamingConsortium(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWirelessControllerHotspot20AnqpRoamingConsortium resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectWirelessControllerHotspot20AnqpRoamingConsortium(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectWirelessControllerHotspot20AnqpRoamingConsortium resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectWirelessControllerHotspot20AnqpRoamingConsortiumRead(d, m)
+}
+
+func resourceObjectWirelessControllerHotspot20AnqpRoamingConsortiumDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectWirelessControllerHotspot20AnqpRoamingConsortium(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectWirelessControllerHotspot20AnqpRoamingConsortium resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectWirelessControllerHotspot20AnqpRoamingConsortiumRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectWirelessControllerHotspot20AnqpRoamingConsortium(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWirelessControllerHotspot20AnqpRoamingConsortium resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectWirelessControllerHotspot20AnqpRoamingConsortium(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectWirelessControllerHotspot20AnqpRoamingConsortium resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectWirelessControllerHotspot20AnqpRoamingConsortiumName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiList(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "comment"
+		if _, ok := i["comment"]; ok {
+			v := flattenObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiListComment(i["comment"], d, pre_append)
+			tmp["comment"] = fortiAPISubPartPatch(v, "ObjectWirelessControllerHotspot20AnqpRoamingConsortium-OiList-Comment")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "index"
+		if _, ok := i["index"]; ok {
+			v := flattenObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiListIndex(i["index"], d, pre_append)
+			tmp["index"] = fortiAPISubPartPatch(v, "ObjectWirelessControllerHotspot20AnqpRoamingConsortium-OiList-Index")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "oi"
+		if _, ok := i["oi"]; ok {
+			v := flattenObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiListOi(i["oi"], d, pre_append)
+			tmp["oi"] = fortiAPISubPartPatch(v, "ObjectWirelessControllerHotspot20AnqpRoamingConsortium-OiList-Oi")
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiListComment(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiListIndex(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiListOi(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectObjectWirelessControllerHotspot20AnqpRoamingConsortium(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("name", flattenObjectWirelessControllerHotspot20AnqpRoamingConsortiumName(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "ObjectWirelessControllerHotspot20AnqpRoamingConsortium-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("oi_list", flattenObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiList(o["oi-list"], d, "oi_list")); err != nil {
+			if vv, ok := fortiAPIPatch(o["oi-list"], "ObjectWirelessControllerHotspot20AnqpRoamingConsortium-OiList"); ok {
+				if err = d.Set("oi_list", vv); err != nil {
+					return fmt.Errorf("Error reading oi_list: %v", err)
+				}
+			} else {
+				return fmt.Errorf("Error reading oi_list: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("oi_list"); ok {
+			if err = d.Set("oi_list", flattenObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiList(o["oi-list"], d, "oi_list")); err != nil {
+				if vv, ok := fortiAPIPatch(o["oi-list"], "ObjectWirelessControllerHotspot20AnqpRoamingConsortium-OiList"); ok {
+					if err = d.Set("oi_list", vv); err != nil {
+						return fmt.Errorf("Error reading oi_list: %v", err)
+					}
+				} else {
+					return fmt.Errorf("Error reading oi_list: %v", err)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectWirelessControllerHotspot20AnqpRoamingConsortiumFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectWirelessControllerHotspot20AnqpRoamingConsortiumName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiList(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "comment"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["comment"], _ = expandObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiListComment(d, i["comment"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "index"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["index"], _ = expandObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiListIndex(d, i["index"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "oi"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["oi"], _ = expandObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiListOi(d, i["oi"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiListComment(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiListIndex(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiListOi(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectWirelessControllerHotspot20AnqpRoamingConsortium(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandObjectWirelessControllerHotspot20AnqpRoamingConsortiumName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("oi_list"); ok {
+		t, err := expandObjectWirelessControllerHotspot20AnqpRoamingConsortiumOiList(d, v, "oi_list")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["oi-list"] = t
+		}
+	}
+
+	return &obj, nil
+}
