363d992cc87566ed108a94ff5a590e388cefe8ae
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:50:04 -0400

    Add resource_securityconsole_install_device

---
+++ fmg/resource_securityconsole_install_device.go
@@ -0,0 +1,341 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Securityconsole InstallDevice
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSecurityconsoleInstallDevice() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSecurityconsoleInstallDeviceUpdate,
+		Read:   resourceSecurityconsoleInstallDeviceRead,
+		Update: resourceSecurityconsoleInstallDeviceUpdate,
+		Delete: resourceSecurityconsoleInstallDeviceDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"force_recreate": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Computed: true,
+			},
+			"fmgadom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"dev_rev_comments": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"flags": &schema.Schema{
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+				Computed: true,
+			},
+			"scope": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"vdom": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"dynamic_sort_subtable": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "false",
+			},
+		},
+	}
+}
+
+func resourceSecurityconsoleInstallDeviceUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "", fmt.Errorf("")
+
+	obj, err := getObjectSecurityconsoleInstallDevice(d)
+	if err != nil {
+		return fmt.Errorf("Error updating SecurityconsoleInstallDevice resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateSecurityconsoleInstallDevice(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating SecurityconsoleInstallDevice resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId("SecurityconsoleInstallDevice")
+
+	return resourceSecurityconsoleInstallDeviceRead(d, m)
+}
+
+func resourceSecurityconsoleInstallDeviceDelete(d *schema.ResourceData, m interface{}) error {
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSecurityconsoleInstallDeviceRead(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func flattenSecurityconsoleInstallDeviceAdom(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSecurityconsoleInstallDeviceDevRevComments(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSecurityconsoleInstallDeviceFlags(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	if v != nil {
+		emap := map[int]string{
+			0:    "none",
+			2:    "preview",
+			4096: "auto_lock_ws",
+		}
+		res := getEnumValbyBit(v, emap)
+		return res
+	}
+	return v
+}
+
+func flattenSecurityconsoleInstallDeviceScope(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			v := flattenSecurityconsoleInstallDeviceScopeName(i["name"], d, pre_append)
+			tmp["name"] = fortiAPISubPartPatch(v, "SecurityconsoleInstallDevice-Scope-Name")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "vdom"
+		if _, ok := i["vdom"]; ok {
+			v := flattenSecurityconsoleInstallDeviceScopeVdom(i["vdom"], d, pre_append)
+			tmp["vdom"] = fortiAPISubPartPatch(v, "SecurityconsoleInstallDevice-Scope-Vdom")
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenSecurityconsoleInstallDeviceScopeName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSecurityconsoleInstallDeviceScopeVdom(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectSecurityconsoleInstallDevice(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("fmgadom", flattenSecurityconsoleInstallDeviceAdom(o["adom"], d, "fmgadom")); err != nil {
+		if vv, ok := fortiAPIPatch(o["adom"], "SecurityconsoleInstallDevice-Adom"); ok {
+			if err = d.Set("fmgadom", vv); err != nil {
+				return fmt.Errorf("Error reading fmgadom: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading fmgadom: %v", err)
+		}
+	}
+
+	if err = d.Set("dev_rev_comments", flattenSecurityconsoleInstallDeviceDevRevComments(o["dev_rev_comments"], d, "dev_rev_comments")); err != nil {
+		if vv, ok := fortiAPIPatch(o["dev_rev_comments"], "SecurityconsoleInstallDevice-DevRevComments"); ok {
+			if err = d.Set("dev_rev_comments", vv); err != nil {
+				return fmt.Errorf("Error reading dev_rev_comments: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading dev_rev_comments: %v", err)
+		}
+	}
+
+	if err = d.Set("flags", flattenSecurityconsoleInstallDeviceFlags(o["flags"], d, "flags")); err != nil {
+		if vv, ok := fortiAPIPatch(o["flags"], "SecurityconsoleInstallDevice-Flags"); ok {
+			if err = d.Set("flags", vv); err != nil {
+				return fmt.Errorf("Error reading flags: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading flags: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("scope", flattenSecurityconsoleInstallDeviceScope(o["scope"], d, "scope")); err != nil {
+			if vv, ok := fortiAPIPatch(o["scope"], "SecurityconsoleInstallDevice-Scope"); ok {
+				if err = d.Set("scope", vv); err != nil {
+					return fmt.Errorf("Error reading scope: %v", err)
+				}
+			} else {
+				return fmt.Errorf("Error reading scope: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("scope"); ok {
+			if err = d.Set("scope", flattenSecurityconsoleInstallDeviceScope(o["scope"], d, "scope")); err != nil {
+				if vv, ok := fortiAPIPatch(o["scope"], "SecurityconsoleInstallDevice-Scope"); ok {
+					if err = d.Set("scope", vv); err != nil {
+						return fmt.Errorf("Error reading scope: %v", err)
+					}
+				} else {
+					return fmt.Errorf("Error reading scope: %v", err)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func flattenSecurityconsoleInstallDeviceFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandSecurityconsoleInstallDeviceAdom(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSecurityconsoleInstallDeviceDevRevComments(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSecurityconsoleInstallDeviceFlags(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return expandStringList(v.(*schema.Set).List()), nil
+}
+
+func expandSecurityconsoleInstallDeviceScope(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["name"], _ = expandSecurityconsoleInstallDeviceScopeName(d, i["name"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "vdom"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["vdom"], _ = expandSecurityconsoleInstallDeviceScopeVdom(d, i["vdom"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandSecurityconsoleInstallDeviceScopeName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSecurityconsoleInstallDeviceScopeVdom(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectSecurityconsoleInstallDevice(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("fmgadom"); ok {
+		t, err := expandSecurityconsoleInstallDeviceAdom(d, v, "fmgadom")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["adom"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("dev_rev_comments"); ok {
+		t, err := expandSecurityconsoleInstallDeviceDevRevComments(d, v, "dev_rev_comments")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["dev_rev_comments"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("flags"); ok {
+		t, err := expandSecurityconsoleInstallDeviceFlags(d, v, "flags")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["flags"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("scope"); ok {
+		t, err := expandSecurityconsoleInstallDeviceScope(d, v, "scope")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["scope"] = t
+		}
+	}
+
+	return &obj, nil
+}
