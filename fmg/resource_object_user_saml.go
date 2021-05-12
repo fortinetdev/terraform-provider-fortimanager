10ea33c9b544bf2712c542cf5e5d21f33b20df78
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:49:13 -0400

    Add resource_object_user_saml

---
+++ fmg/resource_object_user_saml.go
@@ -0,0 +1,527 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: SAML server entry configuration.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectUserSaml() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectUserSamlCreate,
+		Read:   resourceObjectUserSamlRead,
+		Update: resourceObjectUserSamlUpdate,
+		Delete: resourceObjectUserSamlDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"cert": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"entity_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"group_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"idp_cert": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"idp_entity_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"idp_single_logout_url": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"idp_single_sign_on_url": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"single_logout_url": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"single_sign_on_url": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"user_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceObjectUserSamlCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectUserSaml(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectUserSaml resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectUserSaml(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectUserSaml resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectUserSamlRead(d, m)
+}
+
+func resourceObjectUserSamlUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectUserSaml(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectUserSaml resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectUserSaml(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectUserSaml resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectUserSamlRead(d, m)
+}
+
+func resourceObjectUserSamlDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectUserSaml(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectUserSaml resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectUserSamlRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectUserSaml(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectUserSaml resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectUserSaml(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectUserSaml resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectUserSamlCert(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectUserSamlEntityId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectUserSamlGroupName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectUserSamlIdpCert(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectUserSamlIdpEntityId(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectUserSamlIdpSingleLogoutUrl(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectUserSamlIdpSingleSignOnUrl(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectUserSamlName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectUserSamlSingleLogoutUrl(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectUserSamlSingleSignOnUrl(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectUserSamlUserName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectObjectUserSaml(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("cert", flattenObjectUserSamlCert(o["cert"], d, "cert")); err != nil {
+		if vv, ok := fortiAPIPatch(o["cert"], "ObjectUserSaml-Cert"); ok {
+			if err = d.Set("cert", vv); err != nil {
+				return fmt.Errorf("Error reading cert: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading cert: %v", err)
+		}
+	}
+
+	if err = d.Set("entity_id", flattenObjectUserSamlEntityId(o["entity-id"], d, "entity_id")); err != nil {
+		if vv, ok := fortiAPIPatch(o["entity-id"], "ObjectUserSaml-EntityId"); ok {
+			if err = d.Set("entity_id", vv); err != nil {
+				return fmt.Errorf("Error reading entity_id: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading entity_id: %v", err)
+		}
+	}
+
+	if err = d.Set("group_name", flattenObjectUserSamlGroupName(o["group-name"], d, "group_name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["group-name"], "ObjectUserSaml-GroupName"); ok {
+			if err = d.Set("group_name", vv); err != nil {
+				return fmt.Errorf("Error reading group_name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading group_name: %v", err)
+		}
+	}
+
+	if err = d.Set("idp_cert", flattenObjectUserSamlIdpCert(o["idp-cert"], d, "idp_cert")); err != nil {
+		if vv, ok := fortiAPIPatch(o["idp-cert"], "ObjectUserSaml-IdpCert"); ok {
+			if err = d.Set("idp_cert", vv); err != nil {
+				return fmt.Errorf("Error reading idp_cert: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading idp_cert: %v", err)
+		}
+	}
+
+	if err = d.Set("idp_entity_id", flattenObjectUserSamlIdpEntityId(o["idp-entity-id"], d, "idp_entity_id")); err != nil {
+		if vv, ok := fortiAPIPatch(o["idp-entity-id"], "ObjectUserSaml-IdpEntityId"); ok {
+			if err = d.Set("idp_entity_id", vv); err != nil {
+				return fmt.Errorf("Error reading idp_entity_id: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading idp_entity_id: %v", err)
+		}
+	}
+
+	if err = d.Set("idp_single_logout_url", flattenObjectUserSamlIdpSingleLogoutUrl(o["idp-single-logout-url"], d, "idp_single_logout_url")); err != nil {
+		if vv, ok := fortiAPIPatch(o["idp-single-logout-url"], "ObjectUserSaml-IdpSingleLogoutUrl"); ok {
+			if err = d.Set("idp_single_logout_url", vv); err != nil {
+				return fmt.Errorf("Error reading idp_single_logout_url: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading idp_single_logout_url: %v", err)
+		}
+	}
+
+	if err = d.Set("idp_single_sign_on_url", flattenObjectUserSamlIdpSingleSignOnUrl(o["idp-single-sign-on-url"], d, "idp_single_sign_on_url")); err != nil {
+		if vv, ok := fortiAPIPatch(o["idp-single-sign-on-url"], "ObjectUserSaml-IdpSingleSignOnUrl"); ok {
+			if err = d.Set("idp_single_sign_on_url", vv); err != nil {
+				return fmt.Errorf("Error reading idp_single_sign_on_url: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading idp_single_sign_on_url: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenObjectUserSamlName(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "ObjectUserSaml-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("single_logout_url", flattenObjectUserSamlSingleLogoutUrl(o["single-logout-url"], d, "single_logout_url")); err != nil {
+		if vv, ok := fortiAPIPatch(o["single-logout-url"], "ObjectUserSaml-SingleLogoutUrl"); ok {
+			if err = d.Set("single_logout_url", vv); err != nil {
+				return fmt.Errorf("Error reading single_logout_url: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading single_logout_url: %v", err)
+		}
+	}
+
+	if err = d.Set("single_sign_on_url", flattenObjectUserSamlSingleSignOnUrl(o["single-sign-on-url"], d, "single_sign_on_url")); err != nil {
+		if vv, ok := fortiAPIPatch(o["single-sign-on-url"], "ObjectUserSaml-SingleSignOnUrl"); ok {
+			if err = d.Set("single_sign_on_url", vv); err != nil {
+				return fmt.Errorf("Error reading single_sign_on_url: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading single_sign_on_url: %v", err)
+		}
+	}
+
+	if err = d.Set("user_name", flattenObjectUserSamlUserName(o["user-name"], d, "user_name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["user-name"], "ObjectUserSaml-UserName"); ok {
+			if err = d.Set("user_name", vv); err != nil {
+				return fmt.Errorf("Error reading user_name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading user_name: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectUserSamlFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectUserSamlCert(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectUserSamlEntityId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectUserSamlGroupName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectUserSamlIdpCert(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectUserSamlIdpEntityId(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectUserSamlIdpSingleLogoutUrl(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectUserSamlIdpSingleSignOnUrl(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectUserSamlName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectUserSamlSingleLogoutUrl(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectUserSamlSingleSignOnUrl(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectUserSamlUserName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectUserSaml(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("cert"); ok {
+		t, err := expandObjectUserSamlCert(d, v, "cert")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["cert"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("entity_id"); ok {
+		t, err := expandObjectUserSamlEntityId(d, v, "entity_id")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["entity-id"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("group_name"); ok {
+		t, err := expandObjectUserSamlGroupName(d, v, "group_name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["group-name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("idp_cert"); ok {
+		t, err := expandObjectUserSamlIdpCert(d, v, "idp_cert")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["idp-cert"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("idp_entity_id"); ok {
+		t, err := expandObjectUserSamlIdpEntityId(d, v, "idp_entity_id")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["idp-entity-id"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("idp_single_logout_url"); ok {
+		t, err := expandObjectUserSamlIdpSingleLogoutUrl(d, v, "idp_single_logout_url")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["idp-single-logout-url"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("idp_single_sign_on_url"); ok {
+		t, err := expandObjectUserSamlIdpSingleSignOnUrl(d, v, "idp_single_sign_on_url")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["idp-single-sign-on-url"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandObjectUserSamlName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("single_logout_url"); ok {
+		t, err := expandObjectUserSamlSingleLogoutUrl(d, v, "single_logout_url")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["single-logout-url"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("single_sign_on_url"); ok {
+		t, err := expandObjectUserSamlSingleSignOnUrl(d, v, "single_sign_on_url")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["single-sign-on-url"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("user_name"); ok {
+		t, err := expandObjectUserSamlUserName(d, v, "user_name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["user-name"] = t
+		}
+	}
+
+	return &obj, nil
+}
