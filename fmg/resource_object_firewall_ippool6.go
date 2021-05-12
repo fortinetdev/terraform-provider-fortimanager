8489e11d2e34876c2d6b71b1b041b20be690298e
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:48:06 -0400

    Add resource_object_firewall_ippool6

---
+++ fmg/resource_object_firewall_ippool6.go
@@ -0,0 +1,588 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Configure IPv6 IP pools.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceObjectFirewallIppool6() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceObjectFirewallIppool6Create,
+		Read:   resourceObjectFirewallIppool6Read,
+		Update: resourceObjectFirewallIppool6Update,
+		Delete: resourceObjectFirewallIppool6Delete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"scopetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "inherit",
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"adom",
+					"global",
+					"inherit",
+				}, false),
+			},
+			"adom": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"comments": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"dynamic_mapping": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"_scope": &schema.Schema{
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"name": &schema.Schema{
+										Type:     schema.TypeString,
+										Optional: true,
+										Computed: true,
+									},
+									"vdom": &schema.Schema{
+										Type:     schema.TypeString,
+										Optional: true,
+										Computed: true,
+									},
+								},
+							},
+						},
+						"comments": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"endip": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"startip": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"endip": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Optional: true,
+				Computed: true,
+			},
+			"startip": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"dynamic_sort_subtable": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "false",
+			},
+		},
+	}
+}
+
+func resourceObjectFirewallIppool6Create(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectFirewallIppool6(d)
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectFirewallIppool6 resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateObjectFirewallIppool6(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating ObjectFirewallIppool6 resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectFirewallIppool6Read(d, m)
+}
+
+func resourceObjectFirewallIppool6Update(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	obj, err := getObjectObjectFirewallIppool6(d)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectFirewallIppool6 resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateObjectFirewallIppool6(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating ObjectFirewallIppool6 resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, "name"))
+
+	return resourceObjectFirewallIppool6Read(d, m)
+}
+
+func resourceObjectFirewallIppool6Delete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	err = c.DeleteObjectFirewallIppool6(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting ObjectFirewallIppool6 resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceObjectFirewallIppool6Read(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	cfg := m.(*FortiClient).Cfg
+	adomv, err := adomChecking(cfg, d)
+	if err != nil {
+		return fmt.Errorf("Error adom configuration: %v", err)
+	}
+
+	o, err := c.ReadObjectFirewallIppool6(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectFirewallIppool6 resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectObjectFirewallIppool6(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading ObjectFirewallIppool6 resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenObjectFirewallIppool6Comments(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallIppool6DynamicMapping(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "_scope"
+		if _, ok := i["_scope"]; ok {
+			v := flattenObjectFirewallIppool6DynamicMappingScope(i["_scope"], d, pre_append)
+			tmp["_scope"] = fortiAPISubPartPatch(v, "ObjectFirewallIppool6-DynamicMapping-Scope")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "comments"
+		if _, ok := i["comments"]; ok {
+			v := flattenObjectFirewallIppool6DynamicMappingComments(i["comments"], d, pre_append)
+			tmp["comments"] = fortiAPISubPartPatch(v, "ObjectFirewallIppool6-DynamicMapping-Comments")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "endip"
+		if _, ok := i["endip"]; ok {
+			v := flattenObjectFirewallIppool6DynamicMappingEndip(i["endip"], d, pre_append)
+			tmp["endip"] = fortiAPISubPartPatch(v, "ObjectFirewallIppool6-DynamicMapping-Endip")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "startip"
+		if _, ok := i["startip"]; ok {
+			v := flattenObjectFirewallIppool6DynamicMappingStartip(i["startip"], d, pre_append)
+			tmp["startip"] = fortiAPISubPartPatch(v, "ObjectFirewallIppool6-DynamicMapping-Startip")
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenObjectFirewallIppool6DynamicMappingScope(v interface{}, d *schema.ResourceData, pre string) []map[string]interface{} {
+	if v == nil {
+		return nil
+	}
+
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := i["name"]; ok {
+			v := flattenObjectFirewallIppool6DynamicMappingScopeName(i["name"], d, pre_append)
+			tmp["name"] = fortiAPISubPartPatch(v, "ObjectFirewallIppool6DynamicMapping-Scope-Name")
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "vdom"
+		if _, ok := i["vdom"]; ok {
+			v := flattenObjectFirewallIppool6DynamicMappingScopeVdom(i["vdom"], d, pre_append)
+			tmp["vdom"] = fortiAPISubPartPatch(v, "ObjectFirewallIppool6DynamicMapping-Scope-Vdom")
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result
+}
+
+func flattenObjectFirewallIppool6DynamicMappingScopeName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallIppool6DynamicMappingScopeVdom(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallIppool6DynamicMappingComments(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallIppool6DynamicMappingEndip(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallIppool6DynamicMappingStartip(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallIppool6Endip(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallIppool6Name(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenObjectFirewallIppool6Startip(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func refreshObjectObjectFirewallIppool6(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("comments", flattenObjectFirewallIppool6Comments(o["comments"], d, "comments")); err != nil {
+		if vv, ok := fortiAPIPatch(o["comments"], "ObjectFirewallIppool6-Comments"); ok {
+			if err = d.Set("comments", vv); err != nil {
+				return fmt.Errorf("Error reading comments: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading comments: %v", err)
+		}
+	}
+
+	if isImportTable() {
+		if err = d.Set("dynamic_mapping", flattenObjectFirewallIppool6DynamicMapping(o["dynamic_mapping"], d, "dynamic_mapping")); err != nil {
+			if vv, ok := fortiAPIPatch(o["dynamic_mapping"], "ObjectFirewallIppool6-DynamicMapping"); ok {
+				if err = d.Set("dynamic_mapping", vv); err != nil {
+					return fmt.Errorf("Error reading dynamic_mapping: %v", err)
+				}
+			} else {
+				return fmt.Errorf("Error reading dynamic_mapping: %v", err)
+			}
+		}
+	} else {
+		if _, ok := d.GetOk("dynamic_mapping"); ok {
+			if err = d.Set("dynamic_mapping", flattenObjectFirewallIppool6DynamicMapping(o["dynamic_mapping"], d, "dynamic_mapping")); err != nil {
+				if vv, ok := fortiAPIPatch(o["dynamic_mapping"], "ObjectFirewallIppool6-DynamicMapping"); ok {
+					if err = d.Set("dynamic_mapping", vv); err != nil {
+						return fmt.Errorf("Error reading dynamic_mapping: %v", err)
+					}
+				} else {
+					return fmt.Errorf("Error reading dynamic_mapping: %v", err)
+				}
+			}
+		}
+	}
+
+	if err = d.Set("endip", flattenObjectFirewallIppool6Endip(o["endip"], d, "endip")); err != nil {
+		if vv, ok := fortiAPIPatch(o["endip"], "ObjectFirewallIppool6-Endip"); ok {
+			if err = d.Set("endip", vv); err != nil {
+				return fmt.Errorf("Error reading endip: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading endip: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenObjectFirewallIppool6Name(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "ObjectFirewallIppool6-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("startip", flattenObjectFirewallIppool6Startip(o["startip"], d, "startip")); err != nil {
+		if vv, ok := fortiAPIPatch(o["startip"], "ObjectFirewallIppool6-Startip"); ok {
+			if err = d.Set("startip", vv); err != nil {
+				return fmt.Errorf("Error reading startip: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading startip: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenObjectFirewallIppool6FortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandObjectFirewallIppool6Comments(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallIppool6DynamicMapping(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "_scope"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["_scope"], _ = expandObjectFirewallIppool6DynamicMappingScope(d, i["_scope"], pre_append)
+		} else {
+			tmp["_scope"] = make([]string, 0)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "comments"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["comments"], _ = expandObjectFirewallIppool6DynamicMappingComments(d, i["comments"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "endip"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["endip"], _ = expandObjectFirewallIppool6DynamicMappingEndip(d, i["endip"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "startip"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["startip"], _ = expandObjectFirewallIppool6DynamicMappingStartip(d, i["startip"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandObjectFirewallIppool6DynamicMappingScope(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	l := v.([]interface{})
+	if len(l) == 0 || l[0] == nil {
+		return nil, nil
+	}
+
+	result := make([]map[string]interface{}, 0, len(l))
+
+	con := 0
+	for _, r := range l {
+		tmp := make(map[string]interface{})
+		i := r.(map[string]interface{})
+		pre_append := "" // table
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "name"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["name"], _ = expandObjectFirewallIppool6DynamicMappingScopeName(d, i["name"], pre_append)
+		}
+
+		pre_append = pre + "." + strconv.Itoa(con) + "." + "vdom"
+		if _, ok := d.GetOk(pre_append); ok {
+			tmp["vdom"], _ = expandObjectFirewallIppool6DynamicMappingScopeVdom(d, i["vdom"], pre_append)
+		}
+
+		result = append(result, tmp)
+
+		con += 1
+	}
+
+	return result, nil
+}
+
+func expandObjectFirewallIppool6DynamicMappingScopeName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallIppool6DynamicMappingScopeVdom(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallIppool6DynamicMappingComments(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallIppool6DynamicMappingEndip(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallIppool6DynamicMappingStartip(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallIppool6Endip(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallIppool6Name(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandObjectFirewallIppool6Startip(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func getObjectObjectFirewallIppool6(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("comments"); ok {
+		t, err := expandObjectFirewallIppool6Comments(d, v, "comments")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["comments"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("dynamic_mapping"); ok {
+		t, err := expandObjectFirewallIppool6DynamicMapping(d, v, "dynamic_mapping")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["dynamic_mapping"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("endip"); ok {
+		t, err := expandObjectFirewallIppool6Endip(d, v, "endip")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["endip"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandObjectFirewallIppool6Name(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("startip"); ok {
+		t, err := expandObjectFirewallIppool6Startip(d, v, "startip")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["startip"] = t
+		}
+	}
+
+	return &obj, nil
+}
