fa951a4dd9569510deff7104cf142f5c089c4455
Frank Shen <fshen01@fortinet.com>
2021-05-11 21:50:17 -0400

    Add resource_system_certificate_local

---
+++ fmg/resource_system_certificate_local.go
@@ -0,0 +1,338 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Xing Li (@lix-fortinet), Yue Wang (@yuew-ftnt)
+
+// Description: Local keys and certificates.
+
+package fortimanager
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceSystemCertificateLocal() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceSystemCertificateLocalCreate,
+		Read:   resourceSystemCertificateLocalRead,
+		Update: resourceSystemCertificateLocalUpdate,
+		Delete: resourceSystemCertificateLocalDelete,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"certificate": &schema.Schema{
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+				Computed: true,
+			},
+			"comment": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"csr": &schema.Schema{
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"password": &schema.Schema{
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+				Computed: true,
+			},
+			"private_key": &schema.Schema{
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceSystemCertificateLocalCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	obj, err := getObjectSystemCertificateLocal(d)
+	if err != nil {
+		return fmt.Errorf("Error creating SystemCertificateLocal resource while getting object: %v", err)
+	}
+
+	_, err = c.CreateSystemCertificateLocal(obj, adomv, nil)
+
+	if err != nil {
+		return fmt.Errorf("Error creating SystemCertificateLocal resource: %v", err)
+	}
+
+	d.SetId(getStringKey(d, ""))
+
+	return resourceSystemCertificateLocalRead(d, m)
+}
+
+func resourceSystemCertificateLocalUpdate(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	obj, err := getObjectSystemCertificateLocal(d)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemCertificateLocal resource while getting object: %v", err)
+	}
+
+	_, err = c.UpdateSystemCertificateLocal(obj, adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error updating SystemCertificateLocal resource: %v", err)
+	}
+
+	log.Printf(strconv.Itoa(c.Retries))
+
+	d.SetId(getStringKey(d, ""))
+
+	return resourceSystemCertificateLocalRead(d, m)
+}
+
+func resourceSystemCertificateLocalDelete(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	err = c.DeleteSystemCertificateLocal(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error deleting SystemCertificateLocal resource: %v", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceSystemCertificateLocalRead(d *schema.ResourceData, m interface{}) error {
+	mkey := d.Id()
+
+	c := m.(*FortiClient).Client
+	c.Retries = 1
+
+	adomv, err := "global", fmt.Errorf("")
+
+	o, err := c.ReadSystemCertificateLocal(adomv, mkey, nil)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemCertificateLocal resource: %v", err)
+	}
+
+	if o == nil {
+		log.Printf("[WARN] resource (%s) not found, removing from state", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	err = refreshObjectSystemCertificateLocal(d, o)
+	if err != nil {
+		return fmt.Errorf("Error reading SystemCertificateLocal resource from API: %v", err)
+	}
+	return nil
+}
+
+func flattenSystemCertificateLocalCertificate(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return flattenStringList(v)
+}
+
+func flattenSystemCertificateLocalComment(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemCertificateLocalCsr(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return flattenStringList(v)
+}
+
+func flattenSystemCertificateLocalName(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return v
+}
+
+func flattenSystemCertificateLocalPassword(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return flattenStringList(v)
+}
+
+func flattenSystemCertificateLocalPrivateKey(v interface{}, d *schema.ResourceData, pre string) interface{} {
+	return flattenStringList(v)
+}
+
+func refreshObjectSystemCertificateLocal(d *schema.ResourceData, o map[string]interface{}) error {
+	var err error
+
+	if err = d.Set("certificate", flattenSystemCertificateLocalCertificate(o["certificate"], d, "certificate")); err != nil {
+		if vv, ok := fortiAPIPatch(o["certificate"], "SystemCertificateLocal-Certificate"); ok {
+			if err = d.Set("certificate", vv); err != nil {
+				return fmt.Errorf("Error reading certificate: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading certificate: %v", err)
+		}
+	}
+
+	if err = d.Set("comment", flattenSystemCertificateLocalComment(o["comment"], d, "comment")); err != nil {
+		if vv, ok := fortiAPIPatch(o["comment"], "SystemCertificateLocal-Comment"); ok {
+			if err = d.Set("comment", vv); err != nil {
+				return fmt.Errorf("Error reading comment: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading comment: %v", err)
+		}
+	}
+
+	if err = d.Set("csr", flattenSystemCertificateLocalCsr(o["csr"], d, "csr")); err != nil {
+		if vv, ok := fortiAPIPatch(o["csr"], "SystemCertificateLocal-Csr"); ok {
+			if err = d.Set("csr", vv); err != nil {
+				return fmt.Errorf("Error reading csr: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading csr: %v", err)
+		}
+	}
+
+	if err = d.Set("name", flattenSystemCertificateLocalName(o["name"], d, "name")); err != nil {
+		if vv, ok := fortiAPIPatch(o["name"], "SystemCertificateLocal-Name"); ok {
+			if err = d.Set("name", vv); err != nil {
+				return fmt.Errorf("Error reading name: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading name: %v", err)
+		}
+	}
+
+	if err = d.Set("password", flattenSystemCertificateLocalPassword(o["password"], d, "password")); err != nil {
+		if vv, ok := fortiAPIPatch(o["password"], "SystemCertificateLocal-Password"); ok {
+			if err = d.Set("password", vv); err != nil {
+				return fmt.Errorf("Error reading password: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading password: %v", err)
+		}
+	}
+
+	if err = d.Set("private_key", flattenSystemCertificateLocalPrivateKey(o["private-key"], d, "private_key")); err != nil {
+		if vv, ok := fortiAPIPatch(o["private-key"], "SystemCertificateLocal-PrivateKey"); ok {
+			if err = d.Set("private_key", vv); err != nil {
+				return fmt.Errorf("Error reading private_key: %v", err)
+			}
+		} else {
+			return fmt.Errorf("Error reading private_key: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenSystemCertificateLocalFortiTestDebug(d *schema.ResourceData, fosdebugsn int, fosdebugbeg int, fosdebugend int) {
+	log.Printf(strconv.Itoa(fosdebugsn))
+	e := validation.IntBetween(fosdebugbeg, fosdebugend)
+	log.Printf("ER List: %v", e)
+}
+
+func expandSystemCertificateLocalCertificate(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return expandStringList(v.(*schema.Set).List()), nil
+}
+
+func expandSystemCertificateLocalComment(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemCertificateLocalCsr(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return expandStringList(v.(*schema.Set).List()), nil
+}
+
+func expandSystemCertificateLocalName(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return v, nil
+}
+
+func expandSystemCertificateLocalPassword(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return expandStringList(v.(*schema.Set).List()), nil
+}
+
+func expandSystemCertificateLocalPrivateKey(d *schema.ResourceData, v interface{}, pre string) (interface{}, error) {
+	return expandStringList(v.(*schema.Set).List()), nil
+}
+
+func getObjectSystemCertificateLocal(d *schema.ResourceData) (*map[string]interface{}, error) {
+	obj := make(map[string]interface{})
+
+	if v, ok := d.GetOk("certificate"); ok {
+		t, err := expandSystemCertificateLocalCertificate(d, v, "certificate")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["certificate"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("comment"); ok {
+		t, err := expandSystemCertificateLocalComment(d, v, "comment")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["comment"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("csr"); ok {
+		t, err := expandSystemCertificateLocalCsr(d, v, "csr")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["csr"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+		t, err := expandSystemCertificateLocalName(d, v, "name")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["name"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("password"); ok {
+		t, err := expandSystemCertificateLocalPassword(d, v, "password")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["password"] = t
+		}
+	}
+
+	if v, ok := d.GetOk("private_key"); ok {
+		t, err := expandSystemCertificateLocalPrivateKey(d, v, "private_key")
+		if err != nil {
+			return &obj, err
+		} else if t != nil {
+			obj["private-key"] = t
+		}
+	}
+
+	return &obj, nil
+}
