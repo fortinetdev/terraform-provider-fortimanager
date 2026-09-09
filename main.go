f807e9b3f3c4418e4f4ef0834a7088ae33ca1d03
Michael Rosenfeld <72598486+RoseSecurity@users.noreply.github.com>
2026-09-09 11:30:47 -0400

    feat(provider): add debug flag for runtime
    Allow local debugging of the Terraform provider with Delve by
    adding a debug flag and setting the provider address.

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/terraform-providers/terraform-provider-fortimanager/fmg"
+	fortimanager "github.com/terraform-providers/terraform-provider-fortimanager/fmg"
 )
 
 func main() {
+	var debugFlag bool
+
+	flag.BoolVar(&debugFlag, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: fortimanager.Provider})
+		ProviderFunc: fortimanager.Provider,
+		ProviderAddr: "registry.terraform.io/providers/fortinetdev/fortimanager",
+		Debug:        debugFlag,
+	})
 }
